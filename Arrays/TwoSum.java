008674d6717cf4fdfe8b32d463f23eafeb89b4d5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-28 17:23:53 +0530

    Added TwoSum code

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -3,6 +3,7 @@ package Arrays;
 import java.util.Arrays;
 
 /*
+Difficulty = Easy
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up 
 to target.You may assume that each input would have exactly one solution, and you may not use the same element 
 twice.You can return the answer in any order.

e0e6b025703b46a8d4b8de46f1a897be227e1ccd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-28 17:01:34 +0530

    Added TwoSum code

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -28,20 +28,16 @@ public class TwoSum{
 
     static int[] twoSum(int[] nums, int target) {
 
-        int[] index = { -1, -1 };
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length ; j++) {
 
                 if (nums[i] + nums[j] == target) {
-
-                    index[0] = i;
-                    index[1] = j;
-                    return index;
-
+                    return new int[] {i, j};
                 }
+
             }
         }
-        return index;
+        return new int[] {};
     }
 
     public static void main(String[] args) {

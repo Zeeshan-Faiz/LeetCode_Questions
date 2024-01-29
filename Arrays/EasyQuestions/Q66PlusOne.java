02df6deb783d774a6db6135ed01426343eca6fd5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-29 10:57:07 +0530

    Added Q66PlusOne code

--- Arrays/EasyQuestions/Q66PlusOne.java
+++ Arrays/EasyQuestions/Q66PlusOne.java
@@ -40,27 +40,23 @@ public class Q66PlusOne {
 
     static int[] plusOne(int[] digits) {
 
-        int nums = 0;
-        for(int i = 0; i < digits.length; i++){
-            nums = (nums*10) + digits[i];
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        nums = nums + 1;
-        int k = digits.length-1;
-        while(nums > 0){
-            int r = nums % 10;
-            digits[k] = r;
-            k--;
-            nums = nums / 10;
-        }
+    
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
         return digits;
     }
 
     public static void main(String[] args) {
         
-        int[] ar = {9};
+        int[] ar = {1,2,3};
         plusOne(ar);
         System.out.println(Arrays.toString(ar));
     }
-    
-
 }

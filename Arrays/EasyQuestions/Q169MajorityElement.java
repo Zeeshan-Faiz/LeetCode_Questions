30353760f7b699281e27608bfaeae3012bce9d4c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-30 11:06:39 +0530

    Added Q169MajorityElement code

--- Arrays/EasyQuestions/Q169MajorityElement.java
+++ Arrays/EasyQuestions/Q169MajorityElement.java
@@ -25,6 +25,8 @@ public class Q169MajorityElement {
 
     static int majorityElement(int[] nums) {
 
+        /*
+        Brute Force = O(n^2)
         for(int i = 0; i < nums.length; i++){
             int count = 1;
             for(int j = i+1; j < nums.length; j++){
@@ -35,6 +37,25 @@ public class Q169MajorityElement {
                 return nums[i];
         }
         return -1;
+        */
+
+        //Optimized = Boyer-Moore Voting Algorithm (Time Complexity = O(n))
+
+        int maj = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.length; i++){
+
+            if(nums[i] == maj)
+                count++;
+            else
+                count--;
+            
+            if(count == 0){
+                maj = nums[i];
+                count = 1;
+            }
+        }
+        return maj;
     }
 
     public static void main(String[] args) {

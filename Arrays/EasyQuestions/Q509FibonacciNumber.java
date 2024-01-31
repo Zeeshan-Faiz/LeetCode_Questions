d9709ad675dbacdd5c767c94d7785e7cbde5eb53
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-01-31 09:51:09 +0530

    Added Q509FibonacciNumber code

--- Arrays/EasyQuestions/Q509FibonacciNumber.java
+++ Arrays/EasyQuestions/Q509FibonacciNumber.java
@@ -30,5 +30,20 @@ Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 */
 
 public class Q509FibonacciNumber {
+
+    static int fib(int n) {
+
+        int a = 0, b = 1, sum = 0;
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        for(int i = 2; i <= n; i++){
+            sum = a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+    }
     
 }

1040a39d854dd48ac86416746416a2c690150ee0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-01 11:00:02 +0530

    Added Q118PascalsTriangle code

--- Arrays/EasyQuestions/Q118PascalsTriangle.java
+++ Arrays/EasyQuestions/Q118PascalsTriangle.java
@@ -1,5 +1,9 @@
 package Arrays.EasyQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Difficulty = Easy
 Given an integer numRows, return the first numRows of Pascal's triangle.
@@ -22,5 +26,35 @@ Output: [[1]]
 */
 
 public class Q118PascalsTriangle {
+
+    static List<List<Integer>> generate(int numRows) {
+
+    List<List<Integer>> result = new ArrayList<>();
+    if(numRows == 0)
+        return result;
+       
+    result.add(Arrays.asList(1));
+    List<Integer> resultSet;
+    
+    for(int i = 1; i <numRows; i++){
+        resultSet = new ArrayList<>();
+
+        //adding 1's in the corner
+        resultSet.add(1);
+
+        //finding the middle elements
+        List<Integer> prevRow = result.get(i-1);
+        for(int j = 1; j < i; j++){
+            resultSet.add(prevRow.get(j-1) + prevRow.get(j));
+        }
+
+        //adding 1's in the corner
+        resultSet.add(1);
+        result.add(resultSet);
+    }
+    return result;
+    }
+
+    
     
 }

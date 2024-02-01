36babc7a61dc79017655ad3a455de5c7df9dd014
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-01 10:11:47 +0530

    Added Q122BuyAndSellStockII code

--- Arrays/EasyQuestions/Q122BuyAndSellStockII.java
+++ Arrays/EasyQuestions/Q122BuyAndSellStockII.java
@@ -42,5 +42,9 @@ public class Q122BuyAndSellStockII {
         }
         return profit;
     }
-    
+
+    public static void main(String[] args) {
+        
+        System.out.println(maxProfit(new int[] {7,1,5,3,6,4}));
+    }
 }

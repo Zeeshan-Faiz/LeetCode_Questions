d4329aa4567f7570f42092b1c0980277a853c27b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-05 22:26:30 +0530

    Added Q28IndexOfFirstOccurence code

--- Strings/EasyQuestions/Q28IndexOfFirstOccurence.java
+++ Strings/EasyQuestions/Q28IndexOfFirstOccurence.java
@@ -20,12 +20,12 @@ public class Q28IndexOfFirstOccurence {
 
     static int strStr(String haystack, String needle) {
         
-        int len=needle.length();
-        for(int i=0; i<haystack.length(); i++){
-            if (len>haystack.length()) {
+        int len = needle.length();
+        for(int i = 0; i < haystack.length(); i++){
+            if (len > haystack.length()) {
             return -1;
             }
-            String substr=haystack.substring(i,len);
+            String substr = haystack.substring(i,len);
             if(substr.equals(needle)){
                 return i;
             }

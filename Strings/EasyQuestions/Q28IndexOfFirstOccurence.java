4a93b3848c5fec17143b26895c162dd3f65d549c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-05 22:19:31 +0530

    Added Q28IndexOfFirstOccurence code

--- Strings/EasyQuestions/Q28IndexOfFirstOccurence.java
+++ Strings/EasyQuestions/Q28IndexOfFirstOccurence.java
@@ -18,6 +18,20 @@ Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
 public class Q28IndexOfFirstOccurence {
 
-    static
+    static int strStr(String haystack, String needle) {
+        
+        int len=needle.length();
+        for(int i=0; i<haystack.length(); i++){
+            if (len>haystack.length()) {
+            return -1;
+            }
+            String substr=haystack.substring(i,len);
+            if(substr.equals(needle)){
+                return i;
+            }
+            len++;
+        }
+        return -1;
+    }
     
 }

b6d780b08f959d4c3b24a9156445ac63af0a448a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-05 19:12:01 +0530

    Added Q14LongestCommonPrefix code

--- Strings/EasyQuestions/Q14LongestCommonPrefix.java
+++ Strings/EasyQuestions/Q14LongestCommonPrefix.java
@@ -1,5 +1,7 @@
 package Strings.EasyQuestions;
 
+import java.util.Arrays;
+
 /*
 Write a function to find the longest common prefix string amongst an array of strings.
 If there is no common prefix, return an empty string "".
@@ -15,5 +17,22 @@ Explanation: There is no common prefix among the input strings.
 */
 
 public class Q14LongestCommonPrefix{
+
+    static String longestCommonPrefix(String[] strs) {
+
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+
+    }
 
 }

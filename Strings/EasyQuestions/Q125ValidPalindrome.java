38a398c50deae853bf4f4d13c73d76758d0b517c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-05 19:29:55 +0530

    Added Q125ValidPalindrome code

--- Strings/EasyQuestions/Q125ValidPalindrome.java
+++ Strings/EasyQuestions/Q125ValidPalindrome.java
@@ -23,5 +23,30 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 */
 
 public class Q125ValidPalindrome {
+
+    static boolean isPalindrome(String s) {
+        
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
+        }
+        return true;
+    }
     
 }

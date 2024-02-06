25fba8050a8f334d76e746715e9ad39ca78a6403
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-06 22:15:39 +0530

    Added Q2011FindValueOfVariable code

--- Strings/EasyQuestions/Q2011FindValueOfVariable.java
+++ Strings/EasyQuestions/Q2011FindValueOfVariable.java
@@ -8,12 +8,10 @@ There is a programming language with only four operations and one variable X:
 
 Initially, the value of X is 0.
 
-Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
-
- 
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the
+operations.
 
 Example 1:
-
 Input: operations = ["--X","X++","X++"]
 Output: 1
 Explanation: The operations are performed as follows:
@@ -23,7 +21,6 @@ X++: X is incremented by 1, X = -1 + 1 =  0.
 X++: X is incremented by 1, X =  0 + 1 =  1.
 
 Example 2:
-
 Input: operations = ["++X","++X","X++"]
 Output: 3
 Explanation: The operations are performed as follows:
@@ -33,7 +30,6 @@ Initially, X = 0.
 X++: X is incremented by 1, X = 2 + 1 = 3.
 
 Example 3:
-
 Input: operations = ["X++","++X","--X","X--"]
 Output: 0
 Explanation: The operations are performed as follows:
@@ -42,7 +38,6 @@ X++: X is incremented by 1, X = 0 + 1 = 1.
 ++X: X is incremented by 1, X = 1 + 1 = 2.
 --X: X is decremented by 1, X = 2 - 1 = 1.
 X--: X is decremented by 1, X = 1 - 1 = 0.
-
 */
 
 public class Q2011FindValueOfVariable {

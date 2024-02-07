ef56e2309db7abb27ff3a1b828f798d7535feec9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-07 23:03:24 +0530

    Added Q1662EquivalentStrings code

--- Strings/EasyQuestions/Q1662EquivalentStrings.java
+++ Strings/EasyQuestions/Q1662EquivalentStrings.java
@@ -2,13 +2,9 @@ package Strings.EasyQuestions;
 
 /*
 Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
-
 A string is represented by an array if the array elements concatenated in order forms the string.
 
- 
-
 Example 1:
-
 Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
 Output: true
 Explanation:
@@ -17,12 +13,10 @@ word2 represents string "a" + "bc" -> "abc"
 The strings are the same, so return true.
 
 Example 2:
-
 Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
 Output: false
 
 Example 3:
-
 Input: word1  = ["abc", "d", "defg"], w
 */
 

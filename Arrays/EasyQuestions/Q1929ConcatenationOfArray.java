85069a7a1c6f3b6d19340e5114d0a92d568c1615
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-07 23:22:11 +0530

    Added Q1929ConcatenationOfArray code

--- Arrays/EasyQuestions/Q1929ConcatenationOfArray.java
+++ Arrays/EasyQuestions/Q1929ConcatenationOfArray.java
@@ -23,5 +23,15 @@ Explanation: The array ans is formed as follows:
 */
 
 public class Q1929ConcatenationOfArray {
+
+    static int[] getConcatenation(int[] nums) {
+        
+        int[] ans = new int[2 * nums.length];
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
     
 }

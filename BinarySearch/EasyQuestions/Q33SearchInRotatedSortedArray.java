5e70af446020974fd0af217f4c4041702b7fe22b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-09 20:02:10 +0530

    Added Q33SearchInRotatedSortedArray code

--- BinarySearch/EasyQuestions/Q33SearchInRotatedSortedArray.java
+++ BinarySearch/EasyQuestions/Q33SearchInRotatedSortedArray.java
@@ -23,4 +23,30 @@ Output: -1
 
 public class Q33SearchInRotatedSortedArray {
     
+    static int search(int[] arr, int key) {
+        
+        int low = 0, high = arr.length-1, mid = 0;
+        while(low <= high){
+
+            mid = (low+high)/2;
+            if(key == arr[mid])
+                return mid;
+            
+            else if(arr[low] <= arr[mid]){ //left sorted array
+                if(key >= arr[low] && key < arr[mid])
+                    high = mid - 1;
+                else   
+                    low = mid + 1;
+            }
+
+            else{ // right sorted array
+                if(key > arr[mid] && key <= arr[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }

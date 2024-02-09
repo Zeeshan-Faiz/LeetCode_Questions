44ef9a4252352e23486c0dbe427dc601ec9c3b3c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-09 15:14:31 +0530

    Added Q704BinarySearch code

--- BinarySearch/EasyQuestions/Q704BinarySearch.java
+++ BinarySearch/EasyQuestions/Q704BinarySearch.java
@@ -17,5 +17,20 @@ Explanation: 2 does not exist in nums so return -1
 */
 
 public class Q704BinarySearch {
+
+    static int search(int[] nums, int target) {
+        
+        int low = 0, high = nums.length - 1, mid = -1;
+        while(low <= high){
+            mid = low + (high-low)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
     
 }

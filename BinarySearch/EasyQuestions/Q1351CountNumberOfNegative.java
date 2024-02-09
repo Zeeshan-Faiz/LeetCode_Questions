48c9660e5a745e9b9c90d0d0832359c0d70429bc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-09 18:21:46 +0530

    Added Q1351CountNumberOfNegative code

--- BinarySearch/EasyQuestions/Q1351CountNumberOfNegative.java
+++ BinarySearch/EasyQuestions/Q1351CountNumberOfNegative.java
@@ -38,5 +38,10 @@ public class Q1351CountNumberOfNegative {
         }
         return count;
     }
+
+    public static void main(String[] args) {
+        
+        System.out.println(countNegatives(new int[][] {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}}));
+    }
     
 }

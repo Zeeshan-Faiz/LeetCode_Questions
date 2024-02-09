75c487e6538c7728fb943411396e6578e6736007
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-09 20:08:09 +0530

    Added Q33SearchInRotatedSortedArray code

--- BinarySearch/EasyQuestions/Q33SearchInRotatedSortedArray.java
+++ BinarySearch/EasyQuestions/Q33SearchInRotatedSortedArray.java
@@ -7,7 +7,7 @@ that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1
 For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 You must write an algorithm with O(log n) runtime complexity.
-
+ 
 Example 1:
 Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4

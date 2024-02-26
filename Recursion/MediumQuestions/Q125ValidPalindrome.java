10667418490606e43ff35d8a8b569212c7209cc4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-26 21:58:58 +0530

    Added Q125ValidPalindrome code

--- Recursion/MediumQuestions/Q125ValidPalindrome.java
+++ Recursion/MediumQuestions/Q125ValidPalindrome.java
@@ -22,4 +22,32 @@ public class Q125ValidPalindrome{
     Explanation: s is an empty string "" after removing non-alphanumeric characters.
     Since an empty string reads the same forward and backward, it is a palindrome.
     */
+
+    static boolean isPalindrome(String s) {
+        
+        if (s.isEmpty()) 
+        	return true;
+        
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
+        }
+        return true;
+    }
+
+
+
 }

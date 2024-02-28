786e3077e62c6ca213ef5c1b40a6fc954a33a0a1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-28 22:32:54 +0530

    Added Q67QAddBinary code

--- Maths/EasyQuestions/Q67QAddBinary.java
+++ Maths/EasyQuestions/Q67QAddBinary.java
@@ -3,7 +3,15 @@ package Maths.EasyQuestions;
 public class Q67QAddBinary {
 
     /*
-    
+    Given two binary strings a and b, return their sum as a binary string.
+
+    Example 1:
+    Input: a = "11", b = "1"
+    Output: "100"
+
+    Example 2:
+    Input: a = "1010", b = "1011"
+    Output: "10101"
     */
     
 }

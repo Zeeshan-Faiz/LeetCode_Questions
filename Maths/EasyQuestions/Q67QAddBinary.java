2daa286185b6008079d41a2514c6aec9651f5072
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-28 22:34:46 +0530

    Added Q67QAddBinary code

--- Maths/EasyQuestions/Q67QAddBinary.java
+++ Maths/EasyQuestions/Q67QAddBinary.java
@@ -3,34 +3,39 @@ package Maths.EasyQuestions;
 public class Q67QAddBinary {
 
     /*
-    Given two binary strings a and b, return their sum as a binary string.
-
-    Example 1:
-    Input: a = "11", b = "1"
-    Output: "100"
-
-    Example 2:
-    Input: a = "1010", b = "1011"
-    Output: "10101"
-    */
+     * Given two binary strings a and b, return their sum as a binary string.
+     * 
+     * Example 1:
+     * Input: a = "11", b = "1"
+     * Output: "100"
+     * 
+     * Example 2:
+     * Input: a = "1010", b = "1011"
+     * Output: "10101"
+     */
 
     static String addBinary(String a, String b) {
-        
+
         StringBuilder sb = new StringBuilder();
         int carry = 0;
         int i = a.length() - 1;
         int j = b.length() - 1;
 
-        while (i >= 0 || j >= 0 || carry == 1) 
-        {
-        if(i >= 0)
-            carry += a.charAt(i--) - '0';
-        if(j >= 0)
-            carry += b.charAt(j--) - '0';
-        sb.append(carry % 2);
-        carry /= 2;
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0) {
+                carry = carry + a.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                carry = carry + b.charAt(j) - '0';
+                j--;
+            }
+
+            sb.append(carry % 2);
+            carry = carry / 2;
         }
         return sb.reverse().toString();
     }
-    
+
 }

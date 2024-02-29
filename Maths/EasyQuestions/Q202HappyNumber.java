8410c3fe7fe5fa44e0d30a6c2f5029964f2e5ce6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-02-29 22:28:19 +0530

    Added Q202HappyNumbero code

--- Maths/EasyQuestions/Q202HappyNumber.java
+++ Maths/EasyQuestions/Q202HappyNumber.java
@@ -28,12 +28,12 @@ public class Q202HappyNumber {
 
     static boolean isHappy(int n) {
         
-        int slow= n;
-        int fast= n;
+        int slow = n;
+        int fast = n;
         
         do{
-            slow= square(slow); // noral square of a number(moving ahead 1 time)
-            fast= square(square(fast)); // square of sqaure of number(moving ahead 2 times)
+            slow = square(slow); // noral square of a number(moving ahead 1 time)
+            fast = square(square(fast)); // square of sqaure of number(moving ahead 2 times)
             if(slow == 1){
                 return true;
             }
@@ -45,11 +45,11 @@ public class Q202HappyNumber {
     
     static int square(int num){ // taking square of the number
         
-        int ans=0;
+        int ans = 0;
         while(num > 0){
-            int rem= num % 10;
-            ans+= rem*rem;
-            num= num / 10;
+            int rem = num % 10;
+            ans = ans + (rem*rem);
+            num = num / 10;
         }
         return ans; 
     }

025ee6ad5a61673c8b8c98419f28c87a32ff2bdc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-01 22:41:09 +0530

    Added Q268MissingNumber code

--- Maths/EasyQuestions/Q268MissingNumber.java
+++ Maths/EasyQuestions/Q268MissingNumber.java
@@ -25,4 +25,29 @@ public class Q268MissingNumber {
     range since it does not appear in nums.
     */
     
+    static int missingNumber(int[] arr) {
+
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+
+        // case 2
+        return arr;
+    }
+
+    static
+
 }

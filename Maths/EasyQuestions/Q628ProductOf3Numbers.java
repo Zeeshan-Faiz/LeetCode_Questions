df0e84596cadcbffd46d94fe16c96d26f800bf12
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-02 19:32:49 +0530

    Added Q628ProductOf3Numbers code

--- Maths/EasyQuestions/Q628ProductOf3Numbers.java
+++ Maths/EasyQuestions/Q628ProductOf3Numbers.java
@@ -3,7 +3,18 @@ package Maths.EasyQuestions;
 public class Q628ProductOf3Numbers {
 
     /*
-    
+    Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+    Example 1:
+    Input: nums = [1,2,3]
+    Output: 6
+
+    Example 2:
+    Input: nums = [1,2,3,4]
+    Output: 24
+
+    Example 3:
+    Input: nums = [-1,-2,-3]
     */
     
 }

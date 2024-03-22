857dd86df46fa95ee27002357640df40daeaee8d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-22 15:11:52 +0530

    Added Q29DivideTwoIntegers code

--- Maths/MediumQuestions/Q29DivideTwoIntegers.java
+++ Maths/MediumQuestions/Q29DivideTwoIntegers.java
@@ -24,34 +24,28 @@ public class Q29DivideTwoIntegers {
 
     static int divide(int dividend, int divisor) {
 
-        if (divisor == Integer.MIN_VALUE) {
+        if (divisor == Integer.MIN_VALUE) 
             return dividend == Integer.MIN_VALUE ? 1 : 0;
-        }
 
-        if (dividend == 0) {
+        if (dividend == 0) 
             return 0;
-        }
 
         if (dividend == Integer.MIN_VALUE) {
-            if (divisor == 1) {
+            if (divisor == 1) 
                 return Integer.MIN_VALUE;
-            } else if (divisor == -1) {
+            else if (divisor == -1) 
                 return Integer.MAX_VALUE;
-            }
         }
 
         boolean sign = false;
-        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
+        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) 
             sign = true;
-        }
 
-        if (dividend > 0) {
+        if (dividend > 0) 
             dividend = -dividend;
-        }
 
-        if (divisor > 0) {
+        if (divisor > 0) 
             divisor = -divisor;
-        }
 
         int MAX = Integer.MIN_VALUE >> 1;
         int ans = 0;
@@ -66,10 +60,9 @@ public class Q29DivideTwoIntegers {
                 step += step;
             }
 
-            dividend -= temp;
-            ans += step;
+            dividend = dividend - temp;
+            ans = ans + step;
         }
-
         return sign ? ans : -ans;
     }
 

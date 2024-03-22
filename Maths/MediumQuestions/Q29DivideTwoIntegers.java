ed9fe4dac70015726930713aa03b4cc094f5716e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-22 15:01:32 +0530

    Added Q29DivideTwoIntegers code

--- Maths/MediumQuestions/Q29DivideTwoIntegers.java
+++ Maths/MediumQuestions/Q29DivideTwoIntegers.java
@@ -22,4 +22,18 @@ Output: -2
 
 public class Q29DivideTwoIntegers {
     
+    static int divide(int dividend, int divisor) {
+        
+        if(divisor == 0)
+            return 0;
+        if (dividend == -2147483648 && divisor == -1) 
+            return 2147483647;
+        else
+            return dividend / divisor;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(divide(10, 3));
+    }
 }

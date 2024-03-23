bdf43c5ce06c629379a8290ffa04c27d10182be0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-23 08:00:50 +0530

    Added Q237DeleteANode code

--- LinkedList/MeduimQuestions/Q237DeleteANode.java
+++ LinkedList/MeduimQuestions/Q237DeleteANode.java
@@ -17,6 +17,18 @@ Custom testing:
     should not be the last node of the list and should be an actual node in the list.
     We will build the linked list and pass the node to your function.
     The output will be the entire list after calling your function.
+
+Example 1:
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 
+4 -> 1 -> 9 after calling your function.
+
+Example 2:
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 
+4 -> 5 -> 9 after calling your function.
 */
 
 public class Q237DeleteANode {

2a9440eff15bb3f852ba62833e12ad7afe094612
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-24 12:34:00 +0530

    Added Q160IntersectionOfTwoLinkedList code

--- LinkedList/EasyQuestions/Q160IntersectionOfTwoLinkedList.java
+++ LinkedList/EasyQuestions/Q160IntersectionOfTwoLinkedList.java
@@ -36,4 +36,12 @@ public class Q160IntersectionOfTwoLinkedList {
         }
         return ptrA;
     }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 }

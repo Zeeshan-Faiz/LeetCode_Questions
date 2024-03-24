dfd62d555e98f1a56984126b54a6d23d3a2d037a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-24 19:19:18 +0530

    Added Q234PalindromeLinkedList code

--- LinkedList/EasyQuestions/Q234PalindromeLinkedList.java
+++ LinkedList/EasyQuestions/Q234PalindromeLinkedList.java
@@ -10,4 +10,24 @@ Output: true
 
 public class Q234PalindromeLinkedList {
     
+    static boolean isPalindrome(ListNode head) {
+        
+        //Approach : Find the middle node and reverse the 2nd half of the list and compare it with
+        //the first half to find palindrome
+        ListNode mid = middleNode(head);
+        ListNode headSecond = reverseList(mid);
+        ListNode rereverseHead = headSecond;
+
+        // compare both the halves
+        while (head != null && headSecond != null) {
+            if (head.val != headSecond.val) {
+                break;
+            }
+            head = head.next;
+            headSecond = headSecond.next;
+        }
+        reverseList(rereverseHead);
+
+        return head == null || headSecond == null;
+    }
 }

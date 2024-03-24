d76eb1388b6e49ea8139d71c0f8531c16b389018
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-24 11:36:47 +0530

    Added Q83RemoveDuplicatesFromList code

--- LinkedList/EasyQuestions/Q83RemoveDuplicatesFromList.java
+++ LinkedList/EasyQuestions/Q83RemoveDuplicatesFromList.java
@@ -24,4 +24,12 @@ public class Q83RemoveDuplicatesFromList {
         }
         return head;
     }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 }

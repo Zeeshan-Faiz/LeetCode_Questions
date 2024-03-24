a23adcce7d0ee08fd5b0f9c5728cf57456c35174
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-24 19:19:04 +0530

    Added Q234PalindromeLinkedList code

---
+++ LinkedList/EasyQuestions/Q234PalindromeLinkedList.java
@@ -0,0 +1,13 @@
+package LinkedList.EasyQuestions;
+
+/*
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+*/
+
+public class Q234PalindromeLinkedList {
+    
+}

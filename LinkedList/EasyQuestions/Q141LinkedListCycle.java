f10dae3453814b505426d748bb5ba885ad9dde66
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-24 11:46:36 +0530

    Added Q141LinkedListCycle code

--- LinkedList/EasyQuestions/Q141LinkedListCycle.java
+++ LinkedList/EasyQuestions/Q141LinkedListCycle.java
@@ -16,4 +16,18 @@ Explanation: There is a cycle in the linked list, where the tail connects to the
 
 public class Q141LinkedListCycle {
     
+    static boolean hasCycle(ListNode head) {
+      
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

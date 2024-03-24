5c309d1cfdb9752791322b15b769436b3643c4f1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-24 12:03:25 +0530

    Added Q142LinkedListCycleII code

--- LinkedList/MeduimQuestions/Q142LinkedListCycleII.java
+++ LinkedList/MeduimQuestions/Q142LinkedListCycleII.java
@@ -24,8 +24,8 @@ public class Q142LinkedListCycleII {
             fast = fast.next.next;
             slow = slow.next;
             if (fast == slow) {
-                //the moment our two pointers meet, start the slow from head and make both the pointers
-                //move one step, at the end both will meet at the start of the cycle
+                //the moment our two pointers meet, start the slow pointer from head and make both the pointers
+                //move one step at a time, at the end both will meet at the start of the cycle
                 slow = head;
                 while (slow != fast) {
                     slow = slow.next;
@@ -35,5 +35,13 @@ public class Q142LinkedListCycleII {
             }
         }
         return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }

61db378c1a4c82298a7463eb9456cc3f911b5097
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-24 19:19:46 +0530

    Added Q234PalindromeLinkedList code

--- LinkedList/EasyQuestions/Q234PalindromeLinkedList.java
+++ LinkedList/EasyQuestions/Q234PalindromeLinkedList.java
@@ -43,4 +43,24 @@ public class Q234PalindromeLinkedList {
         return s;
     }
 
+    //reversing a list
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+
 }

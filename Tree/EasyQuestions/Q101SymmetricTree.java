166dfdb76b5ea1033621d5f33f581e0cdcae9854
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-27 19:48:46 +0530

    Added Q101SymmetricTree code

---
+++ Tree/EasyQuestions/Q101SymmetricTree.java
@@ -0,0 +1,14 @@
+package Tree.EasyQuestions;
+
+/*
+Given the root of a binary tree, check whether it is a mirror of itself 
+(i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+*/
+
+public class Q101SymmetricTree {
+    
+}

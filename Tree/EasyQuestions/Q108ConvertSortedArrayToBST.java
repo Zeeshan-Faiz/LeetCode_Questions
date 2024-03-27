f70773723bc0ef3a65b7ae3720a95a65f3b971b2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-27 20:06:53 +0530

    Added Q108ConvertSortedArrayToBST code

--- Tree/EasyQuestions/Q108ConvertSortedArrayToBST.java
+++ Tree/EasyQuestions/Q108ConvertSortedArrayToBST.java
@@ -35,4 +35,17 @@ public class Q108ConvertSortedArrayToBST {
                                                   // array
         return root;
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

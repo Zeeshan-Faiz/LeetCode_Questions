10d2821fc5edb103e3ae994b126f1d44051f6130
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-28 16:06:27 +0530

    Added Q104MaxDepthOfBinaryTree code

--- Tree/EasyQuestions/Q104MaxDepthOfBinaryTree.java
+++ Tree/EasyQuestions/Q104MaxDepthOfBinaryTree.java
@@ -25,4 +25,17 @@ public class Q104MaxDepthOfBinaryTree {
 
         return depth;
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

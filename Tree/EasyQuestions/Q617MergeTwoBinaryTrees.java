c4377d6ce34a1a9c6f0acc7f465edb2b8687fff6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-28 15:53:37 +0530

    Added Q617MergeTwoBinaryTrees code

--- Tree/EasyQuestions/Q617MergeTwoBinaryTrees.java
+++ Tree/EasyQuestions/Q617MergeTwoBinaryTrees.java
@@ -35,4 +35,17 @@ public class Q617MergeTwoBinaryTrees {
 
         return root1;
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

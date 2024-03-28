e88d4ebe40ca2c8e01cba1a3534a0ecc03b5d4e7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-28 16:22:47 +0530

    Added Q257BinaryTreePaths code

--- Tree/EasyQuestions/Q257BinaryTreePaths.java
+++ Tree/EasyQuestions/Q257BinaryTreePaths.java
@@ -13,12 +13,30 @@ Output: ["1->2->5","1->3"]
 */
 
 public class Q257BinaryTreePaths {
-    
+
     List<String> ans = new ArrayList<String>();
+
     public List<String> binaryTreePaths(TreeNode root) {
 
         StringBuilder sb = new StringBuilder();
-        loop(root,sb);
+        loop(root, sb);
         return ans;
+    }
+
+    private void loop(TreeNode root, StringBuilder sb) {
+        if (root == null)
+            return;
+        int length = sb.length();
+
+        // Append the current node's value to the path.
+        sb.append(root.val);
+        // If it's a leaf node, add the path to the result list.
+        if (root.left == null && root.right == null) {
+            ans.add(sb.toString());
+        }
+        sb.append("->");
+        loop(root.left, sb);
+        loop(root.right, sb);
+        sb.setLength(length);
     }
 }

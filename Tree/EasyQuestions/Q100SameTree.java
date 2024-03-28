01803353e6e4236017e7bd491879aa61a9a099ad
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-28 16:46:55 +0530

    Added Q100SameTree code

--- Tree/EasyQuestions/Q100SameTree.java
+++ Tree/EasyQuestions/Q100SameTree.java
@@ -6,7 +6,8 @@ are the same or not. Two binary trees are considered the same if they are struct
 and the nodes have the same value.
 
 Example 1:
-
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
 */
 
 public class Q100SameTree {

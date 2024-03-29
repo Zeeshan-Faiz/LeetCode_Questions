e7c8a174c8f9c8af61d0402d87384a8447098af7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-29 16:50:20 +0530

    Added Q112PathSum code

--- Tree/EasyQuestions/Q112PathSum.java
+++ Tree/EasyQuestions/Q112PathSum.java
@@ -12,5 +12,18 @@ Output: true
 Explanation: The root-to-leaf path with the target sum is shown.
 */
 public class Q112PathSum {
-    
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+        // check whether the value of the leaf node is equal to the remaining targetSum.
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        // check for a valid path with the target sum in both the left and right subtrees
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+
+    }
 }

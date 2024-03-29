0cf4ece67b2480e4e521f6e071883170365e9c83
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-29 16:29:15 +0530

    Added Q235LowestCommonAncestor code

--- Tree/MediumQuestions/Q235LowestCommonAncestor.java
+++ Tree/MediumQuestions/Q235LowestCommonAncestor.java
@@ -16,4 +16,22 @@ Explanation: The LCA of nodes 2 and 8 is 6.
 
 public class Q235LowestCommonAncestor {
     
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if ( root == null || root == p || root == q ) 
+           return root;
+       
+       TreeNode left = lowestCommonAncestor(root.left, p, q);
+       TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+       // if Left is NULL, Right will be our answer
+       if ( left == null ) return right;
+
+       // if Rigth is NULL, Left be our answer
+       else if ( right == null ) return left;
+
+       // if both are NOT NULL, then that node is our answer 
+       else 
+           return root;
+   }
 }

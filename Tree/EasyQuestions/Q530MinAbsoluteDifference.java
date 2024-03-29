1fdf1c51f1d8e4100361d9c743ef44c018f694cf
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-29 17:08:58 +0530

    Added Q530MinAbsoluteDifference code

--- Tree/EasyQuestions/Q530MinAbsoluteDifference.java
+++ Tree/EasyQuestions/Q530MinAbsoluteDifference.java
@@ -7,6 +7,10 @@ values of any two different nodes in the tree.
 Example 1:
 Input: root = [4,2,6,1,3]
 Output: 1
+
+Example 2:
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
 */
 
 public class Q530MinAbsoluteDifference {

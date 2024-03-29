5e78484a51fe9778a5069f1152864775e8d40ccb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-29 16:50:39 +0530

    Added Q112PathSum code

--- Tree/EasyQuestions/Q112PathSum.java
+++ Tree/EasyQuestions/Q112PathSum.java
@@ -24,6 +24,18 @@ public class Q112PathSum {
         }
         // check for a valid path with the target sum in both the left and right subtrees
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
 
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

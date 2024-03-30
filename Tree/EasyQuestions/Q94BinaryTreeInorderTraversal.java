6b323efec6381b726c4d1e65dce1e69cbf117ac2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-30 13:04:48 +0530

    Added Q94BinaryTreeInorderTraversal code

---
+++ Tree/EasyQuestions/Q94BinaryTreeInorderTraversal.java
@@ -0,0 +1,13 @@
+package Tree.EasyQuestions;
+
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+*/
+
+public class Q94BinaryTreeInorderTraversal {
+    
+}

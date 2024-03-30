8d839d6819f99fbe1c60000183bffb2ffa24239d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-03-30 13:05:28 +0530

    Added Q94BinaryTreeInorderTraversal code

--- Tree/EasyQuestions/Q94BinaryTreeInorderTraversal.java
+++ Tree/EasyQuestions/Q94BinaryTreeInorderTraversal.java
@@ -26,5 +26,16 @@ public class Q94BinaryTreeInorderTraversal {
         return res;
     }
 
-    
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

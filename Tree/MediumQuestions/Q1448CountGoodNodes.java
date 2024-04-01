b6dfd642c98ff5b5f777bbaddcb6b759751c5cfd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-01 11:26:39 +0530

    Added Q1448CountGoodNodes code

---
+++ Tree/MediumQuestions/Q1448CountGoodNodes.java
@@ -0,0 +1,11 @@
+package Tree.MediumQuestions;
+
+/*
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there 
+are no nodes with a value greater than X.
+Return the number of good nodes in the binary tree.
+*/
+
+public class Q1448CountGoodNodes {
+    
+}

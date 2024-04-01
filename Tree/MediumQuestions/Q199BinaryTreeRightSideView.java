b6c3be8dd38f7195ad564e6be7753a47bcc40249
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-01 17:37:14 +0530

    Added Q199BinaryTreeRightSideView code

--- Tree/MediumQuestions/Q199BinaryTreeRightSideView.java
+++ Tree/MediumQuestions/Q199BinaryTreeRightSideView.java
@@ -17,7 +17,7 @@ Output: [1,3]
 */
 
 public class Q199BinaryTreeRightSideView {
-    
+
     public List<Integer> rightSideView(TreeNode root) {
 
         List<Integer> result = new ArrayList<Integer>();
@@ -25,16 +25,35 @@ public class Q199BinaryTreeRightSideView {
         return result;
     }
 
-    public void helper(TreeNode curr, List<Integer> result, int currDepth){
-        
-        if(curr == null)
+    public void helper(TreeNode curr, List<Integer> result, int currDepth) {
+
+        if (curr == null)
             return;
-        
-        if(currDepth == result.size()){
+
+        if (currDepth == result.size()) {
             result.add(curr.val);
-        
-        
-        helper(curr.right, result, currDepth + 1);
-        helper(curr.left, result, currDepth + 1);
+
+            helper(curr.right, result, currDepth + 1);
+            helper(curr.left, result, currDepth + 1);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

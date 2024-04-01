6bbe95f631dc103c161edfacaf0ac773a5a37f35
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-01 17:17:20 +0530

    Added Q236LowestCommonAncestorII code

--- Tree/MediumQuestions/Q236LowestCommonAncestorII.java
+++ Tree/MediumQuestions/Q236LowestCommonAncestorII.java
@@ -7,6 +7,10 @@ two nodes p and q as the lowest node in T that has both p and q as descendants (
 node to be a descendant of itself).”
 
 Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+
 
 */
 

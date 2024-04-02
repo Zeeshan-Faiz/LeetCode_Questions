87bad72d95dae46e4827ebd5c76c365c4b694269
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-02 11:59:46 +0530

    Added Q103BinaryZigZagLevelOrderTraversal code

--- Tree/MediumQuestions/Q103BinaryZigZagLevelOrderTraversal.java
+++ Tree/MediumQuestions/Q103BinaryZigZagLevelOrderTraversal.java
@@ -1,5 +1,10 @@
 package Tree.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /*
 Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from 
 left to right, then right to left for the next level and alternate between)
@@ -11,4 +16,37 @@ Output: [[3],[20,9],[15,7]]
 
 public class Q103BinaryZigZagLevelOrderTraversal {
     
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null)
+            return new ArrayList<>();
+        
+        queue.offer(root);
+        int level = 0;
+        while(!queue.isEmpty()){
+            
+            int len = queue.size();
+            List<Integer> ans = new ArrayList<>();
+            
+            for(int i=0;i<len;i++){
+                TreeNode temp=queue.poll();
+                if(temp.left!=null)
+                    queue.add(temp.left);
+                
+                if(temp.right!=null)
+                    queue.add(temp.right);
+                
+                //cleverly add the nodes in the list for different levels
+                if(level%2==0)
+                    ans.add(temp.val);
+                else
+                    ans.add(0,temp.val);
+            }
+            level++;
+            result.add(ans);
+        }
+        return result;
+    }
 }

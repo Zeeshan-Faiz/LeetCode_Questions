5996ce3e87095bd243cdf8a1e624865717e90c30
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-03 13:50:00 +0530

    Added Q99RecoverBinarySearchTree code

--- Tree/MediumQuestions/Q99RecoverBinarySearchTree.java
+++ Tree/MediumQuestions/Q99RecoverBinarySearchTree.java
@@ -20,25 +20,46 @@ public class Q99RecoverBinarySearchTree {
     TreeNode prev = null, first = null, second = null;
 
     public void recoverTree(TreeNode root) {
+        
         if (root == null)
             return;
+        
         inorder(root);
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
     }
 
-    //Inorder of BST gives sorted array, from this we can find the two wrong nodes
-    void inorder(TreeNode root){
-        if(root==null)
+    // Inorder of BST gives sorted array, from this we can find the two wrong nodes
+    void inorder(TreeNode root) {
+        
+        if (root == null)
             return;
+        
         inorder(root.left);
-        if(prev!=null&&root.val<prev.val){
-            if(first==null)
-                first=prev;
-            second=root;
+        if (prev != null && root.val < prev.val) {
+            if (first == null)
+                first = prev;
+            second = root;
         }
-        prev=root;
+        prev = root;
         inorder(root.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

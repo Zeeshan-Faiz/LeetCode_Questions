5db2cff068046e015804deb6dd171ae41901116e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-04 11:40:02 +0530

    Added Q114FlattenBinaryTreeToLinkedList code

--- Tree/MediumQuestions/Q114FlattenBinaryTreeToLinkedList.java
+++ Tree/MediumQuestions/Q114FlattenBinaryTreeToLinkedList.java
@@ -13,5 +13,32 @@ Output: [1,null,2,null,3,null,4,null,5,null,6]
 */
 
 public class Q114FlattenBinaryTreeToLinkedList {
-    
+
+    public void flatten(TreeNode root) {
+        
+        if (root == null)
+            return;
+
+        TreeNode current = root;
+        while (current != null) {
+            if (current.left != null) {
+                TreeNode temp = current.left;
+                // Find the rightmost null value for this current.left node
+                while (temp.right != null) {
+                    temp = temp.right;
+                }
+
+                // add all the left sub-tree to the right most node having null value to it's right
+                temp.right = current.right;
+
+                // add all the shifted nodes to right of the current node
+                current.right = current.left;
+
+                // make all the left side of the node as null
+                current.left = null;
+            }
+            // move on to the next right node
+            current = current.right;
+        }
+    }
 }

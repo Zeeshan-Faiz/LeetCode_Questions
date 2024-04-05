e34483ebf70be90535d74afaafa9d13bac3187a6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-05 19:32:38 +0530

    Added Q230KthSmallestElementInBST code

--- Tree/MediumQuestions/Q230KthSmallestElementInBST.java
+++ Tree/MediumQuestions/Q230KthSmallestElementInBST.java
@@ -36,4 +36,17 @@ public class Q230KthSmallestElementInBST {
         }
         inOrder(root.right, k);
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

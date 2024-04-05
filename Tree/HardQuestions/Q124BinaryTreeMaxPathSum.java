5259e0d4904cf05572c5ddeaebe14b1a4e6ae2be
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-05 19:44:59 +0530

    Added Q124BinaryTreeMaxPathSum code

--- Tree/HardQuestions/Q124BinaryTreeMaxPathSum.java
+++ Tree/HardQuestions/Q124BinaryTreeMaxPathSum.java
@@ -6,6 +6,14 @@ has an edge connecting them. A node can only appear in the sequence at most once
 path does not need to pass through the root.
 The path sum of a path is the sum of the node's values in the path. Given the root of a binary tree, 
 return the maximum path sum of any non-empty path.
+
+Example 1:
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+
+Example 2:
+
 */
 
 public class Q124BinaryTreeMaxPathSum {

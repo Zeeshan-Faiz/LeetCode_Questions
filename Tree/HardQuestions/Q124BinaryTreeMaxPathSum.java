513d074fa022247f2094576cc5a1a383b18dc223
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-05 19:46:55 +0530

    Added Q124BinaryTreeMaxPathSum code

--- Tree/HardQuestions/Q124BinaryTreeMaxPathSum.java
+++ Tree/HardQuestions/Q124BinaryTreeMaxPathSum.java
@@ -27,7 +27,7 @@ public class Q124BinaryTreeMaxPathSum {
         return ans;
     }
 
-    public void helper(TreeNode root){
+    public int helper(TreeNode node){
 
         if(node == null)
             return 0;
@@ -43,5 +43,18 @@ public class Q124BinaryTreeMaxPathSum {
         ans = Math.max(ans, pathSum);
 
         return Math.max(left, right) + node.val;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

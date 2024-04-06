0f9231b81725985d129e2370d1f641022535e8f7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-06 17:16:15 +0530

    Added Q49GroupAnagrams code

--- Strings/MediumQuestions/Q49GroupAnagrams.java
+++ Strings/MediumQuestions/Q49GroupAnagrams.java
@@ -1,5 +1,11 @@
 package Strings.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
@@ -19,5 +25,27 @@ Output: [["a"]]
 */
 
 public class Q49GroupAnagrams {
-    
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) 
+        {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            // Use sorted strings as keys to group anagrams efficiently.
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            // if any new word after sorting matches the already sorted word in map, it's anagram
+            map.get(sortedWord).add(word);
+        }
+
+        // return all map values
+        return new ArrayList<>(map.values());
+    }
 }

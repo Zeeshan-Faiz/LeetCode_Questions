3df3a485d73bf8620eef5dd80d52cc676e73500c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-06 17:15:42 +0530

    Added Q49GroupAnagrams code

--- Strings/MediumQuestions/Q49GroupAnagrams.java
+++ Strings/MediumQuestions/Q49GroupAnagrams.java
@@ -6,6 +6,16 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 typically using all the original letters exactly once.
 
 Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
 */
 
 public class Q49GroupAnagrams {

48cb5dac2b2f4cbd5173374fe616d0e1f51ac429
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-06 16:34:28 +0530

    Added Q5LongestPalindromicString code

--- Strings/MediumQuestions/Q5LongestPalindromicString.java
+++ Strings/MediumQuestions/Q5LongestPalindromicString.java
@@ -1,5 +1,18 @@
 package Strings.MediumQuestions;
 
+/*
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+*/
+
 public class Q5LongestPalindromicString {
     
 }

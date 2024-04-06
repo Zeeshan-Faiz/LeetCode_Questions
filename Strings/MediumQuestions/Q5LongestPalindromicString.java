6d9748906cc04c6de0a03acf89e27110a017d7f1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-06 16:34:54 +0530

    Added Q5LongestPalindromicString code

--- Strings/MediumQuestions/Q5LongestPalindromicString.java
+++ Strings/MediumQuestions/Q5LongestPalindromicString.java
@@ -14,5 +14,22 @@ Output: "bb"
 */
 
 public class Q5LongestPalindromicString {
-    
+
+    int maxLen = 0;// length of longest palindrome
+    int lo = 0;
+
+    public String longestPalindrome(String s) {
+
+        if (s.length() < 2)
+            return s;
+
+        char[] input = s.toCharArray();
+
+        // i act as a centre of palindrome
+        for (int i = 0; i < input.length; i++) {
+            expandPalindrome(input, i, i);
+            expandPalindrome(input, i, i + 1);
+        }
+        return s.substring(lo, lo + maxLen);
+    }
 }

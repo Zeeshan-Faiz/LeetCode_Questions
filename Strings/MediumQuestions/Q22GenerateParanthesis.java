8a624f9bfdd236c2344f57a3435cbc443b4542fc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-07 14:59:45 +0530

    Added Q22GenerateParanthesis code

--- Strings/MediumQuestions/Q22GenerateParanthesis.java
+++ Strings/MediumQuestions/Q22GenerateParanthesis.java
@@ -3,18 +3,13 @@ package Strings.MediumQuestions;
 /*
 Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
- 
-
 Example 1:
-
 Input: n = 3
 Output: ["((()))","(()())","(())()","()(())","()()()"]
 
 Example 2:
-
 Input: n = 1
 Output: ["()"]
-
 */
 
 public class Q22GenerateParanthesis {

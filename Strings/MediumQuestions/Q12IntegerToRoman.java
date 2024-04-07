8a0a5bdc5e16909489cb0bc9503ceccdfb13f5f0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-07 15:51:15 +0530

    Added Q12IntegerToRoman code

--- Strings/MediumQuestions/Q12IntegerToRoman.java
+++ Strings/MediumQuestions/Q12IntegerToRoman.java
@@ -42,5 +42,18 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 */
 
 public class Q12IntegerToRoman {
-    
+
+    private static final int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    private static final String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return sb.toString();
+    }
 }

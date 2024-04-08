3342c3784a7fc6dcd5099562d2fc4ef7b981f211
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-08 15:07:14 +0530

    Added Q273IntegerToEnglishWord code

--- Strings/HardQuestions'/Q273IntegerToEnglishWord.java
+++ Strings/HardQuestions'/Q273IntegerToEnglishWord.java
@@ -33,15 +33,20 @@ public class Q273IntegerToEnglishWord {
         StringBuilder result = new StringBuilder();
         if (num < 20) {
             result.append(belowTwenty[num]);
-        } else if (num < 100) {
+        } 
+        else if (num < 100) {
             result.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
-        } else if (num < 1000) {
+        } 
+        else if (num < 1000) {
             result.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
-        } else if (num < 1000000) {
+        } 
+        else if (num < 1000000) {
             result.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
-        } else if (num < 1000000000) {
+        } 
+        else if (num < 1000000000) {
             result.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
-        } else {
+        } 
+        else {
             result.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
         }
         return result.toString().trim();

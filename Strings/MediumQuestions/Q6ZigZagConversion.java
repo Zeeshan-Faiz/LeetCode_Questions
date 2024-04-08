a699c3dd52dd170b7c47c973bbd16e1abc05d3a0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-08 14:46:31 +0530

    Added Q6ZigZagConversion code

--- Strings/MediumQuestions/Q6ZigZagConversion.java
+++ Strings/MediumQuestions/Q6ZigZagConversion.java
@@ -20,5 +20,27 @@ P     I
 */
 
 public class Q6ZigZagConversion {
-    
+
+    public String convert(String s, int numRows) {
+        
+        int length = s.length();
+        if (length == numRows || numRows == 1)
+            return s;
+
+        char[] ans = new char[length];
+        int cnt = 0;
+        for (int i = 0; i < numRows; i++) {
+            int j = i, k = 0, m = 2 * (numRows - 1);
+            while (j < length) {
+                ans[cnt++] = s.charAt(j);
+                if (i == 0 || ((i + j) % m) != 0) {
+                    k = (numRows - (i + 1)) * 2;
+                } else {
+                    k = 2 * i;
+                }
+                j = j + k;
+            }
+        }
+        return new String(ans);
+    }
 }

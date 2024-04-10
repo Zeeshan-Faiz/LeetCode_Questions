6cdb9e45a633b54d12176d5ed108f1f6c4cb06e1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-10 11:01:58 +0530

    Added Q560SubarraySumEqualsK code

--- Arrays/MeduimQuestions/Q560SubarraySumEqualsK.java
+++ Arrays/MeduimQuestions/Q560SubarraySumEqualsK.java
@@ -1,5 +1,7 @@
 package Arrays.MeduimQuestions;
 
+import java.util.HashMap;
+
 /*
 Given an array of integers nums and an integer k, return the total number of subarrays whose 
 sum equals to k. A subarray is a contiguous non-empty sequence of elements within an array.
@@ -14,5 +16,27 @@ Output: 2
 */
 
 public class Q560SubarraySumEqualsK {
-    
+
+    public int subarraySum(int[] nums, int k) {
+        
+        int[] prevSum = new int[nums.length + 1];
+        prevSum[0] = 0;
+        int sum = 0, result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            prevSum[i + 1] = sum;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < prevSum.length; i++) {
+            // find number of pairs where difference = k
+            if (map.containsKey(prevSum[i] - k)) {
+                result += map.get(prevSum[i] - k);
+            }
+            map.put(prevSum[i], map.getOrDefault(prevSum[i], 0) + 1);
+        }
+        return result;
+    }
 }

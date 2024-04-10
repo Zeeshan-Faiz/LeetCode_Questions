1f276424e76da6d913a8d54bf605118a6ab6f0a3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-10 15:09:12 +0530

    Added Q16ThreeSumClosest code

--- Arrays/MeduimQuestions/Q16ThreeSumClosest.java
+++ Arrays/MeduimQuestions/Q16ThreeSumClosest.java
@@ -1,5 +1,14 @@
 package Arrays.MeduimQuestions;
 
+/*
+Given an integer array nums of length n and an integer target, find three integers in nums such
+that the sum is closest to target. Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+
+*/
+
 public class Q16ThreeSumClosest {
     
 }

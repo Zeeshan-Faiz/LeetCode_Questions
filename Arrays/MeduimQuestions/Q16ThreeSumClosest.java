fe4de8a08dd530aabca848e9ad8e48ffe6563f68
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-10 15:09:47 +0530

    Added Q16ThreeSumClosest code

--- Arrays/MeduimQuestions/Q16ThreeSumClosest.java
+++ Arrays/MeduimQuestions/Q16ThreeSumClosest.java
@@ -1,5 +1,7 @@
 package Arrays.MeduimQuestions;
 
+import java.util.Arrays;
+
 /*
 Given an integer array nums of length n and an integer target, find three integers in nums such
 that the sum is closest to target. Return the sum of the three integers.
@@ -18,4 +20,30 @@ Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
 
 public class Q16ThreeSumClosest {
     
+    public int threeSumClosest(int[] nums, int target) {
+        int len = nums.length;
+        Arrays.sort(nums);
+        if(target<=nums[0]+nums[1]+nums[2]) return nums[0]+nums[1]+nums[2];
+        if(target>=nums[len-1]+nums[len-2]+nums[len-3]) return nums[len-1]+nums[len-2]+nums[len-3];
+        int minSum = Integer.MAX_VALUE;
+        int closest = 0;
+        // System.out.println(Arrays.toString(nums));
+        for(int i=0; i<nums.length-2; i++) {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k = nums.length-1;
+            while(j<k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(minSum>Math.abs(target-sum)) { 
+                    // System.out.println(nums[i] + " " + nums[j] + " " + nums[k]);
+                    minSum = Math.abs(target-sum);
+                    if(minSum == 0) return sum;
+                    closest = sum;
+                }
+                if(sum<target) j++;
+                else k--;
+            }
+        }
+        return closest;
+    }
 }

29e61e0eec74832189299e7dcb50c036ba5b9ebb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-13 09:16:26 +0530

    Added Q46Permutations code

---
+++ Recursion/MediumQuestions/Q46Permutations.java
@@ -0,0 +1,10 @@
+package Recursion.MediumQuestions;
+
+/*
+Given an array nums of distinct integers, return all the possible permutations. 
+You can return the answer in any order.
+*/
+
+public class Q46Permutations {
+    
+}

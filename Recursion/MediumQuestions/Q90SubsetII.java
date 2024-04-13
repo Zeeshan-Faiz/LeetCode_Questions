a4812bb7d8df207ac3636f35a2ff69f767c1d197
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-13 09:40:29 +0530

    Added Q90SubsetII code

--- Recursion/MediumQuestions/Q90SubsetII.java
+++ Recursion/MediumQuestions/Q90SubsetII.java
@@ -2,6 +2,7 @@ package Recursion.MediumQuestions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /*
@@ -22,8 +23,21 @@ public class Q90SubsetII {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
+        LinkedList<Integer> list = new LinkedList<>();
         subSet(nums, 0, ans, list);
         return ans;
+    }
+
+    public void subSet(int[] nums,int idx,List<List<Integer>> ans,LinkedList<Integer> list) {
+        
+        ans.add(new ArrayList<>(list));
+
+        for (int i = idx; i < nums.length; i++) {
+            //taking care of the duplicate elements
+            if (i > idx && nums[i] == nums[i - 1]) continue;
+            list.add(nums[i]);
+            subSet(nums, i + 1, ans, list);
+            list.removeLast();
+        }
     }
 }

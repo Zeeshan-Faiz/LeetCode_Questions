b4d090375195de399756e36b374a8fb721c824a5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-13 09:17:15 +0530

    Added Q46Permutations code

--- Recursion/MediumQuestions/Q46Permutations.java
+++ Recursion/MediumQuestions/Q46Permutations.java
@@ -29,4 +29,30 @@ public class Q46Permutations {
         recurpermute(0,nums,ans);
         return ans;
     }
+
+    public void recurpermute(int index,int[]nums,List<List<Integer>> ans)
+    {
+        if(index == nums.length)
+        {
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++)
+            {
+                ds.add(nums[i]);
+            }
+            ans.add(/*new ArrayList<>(*/ds);
+            return;
+        }
+        for(int i=index;i<nums.length;i++)
+        {
+            swap(i,index,nums);
+            recurpermute(index+1,nums,ans);
+            swap(i,index,nums);
+        }
+    }
+    public void swap(int index1,int index,int[] nums)
+    {
+        int temp = nums[index];
+        nums[index] = nums[index1];
+        nums[index1] = temp;
+    }
 }

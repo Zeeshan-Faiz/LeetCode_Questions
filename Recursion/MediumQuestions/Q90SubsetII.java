fbd4cf92b67edfc7b03373dd061b8e777a2446f7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-13 09:36:44 +0530

    Added Q90SubsetII code

--- Recursion/MediumQuestions/Q90SubsetII.java
+++ Recursion/MediumQuestions/Q90SubsetII.java
@@ -1,5 +1,9 @@
 package Recursion.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
 The solution set must not contain duplicate subsets. Return the solution in any order.
@@ -15,4 +19,11 @@ Output: [[],[0]]
 
 public class Q90SubsetII {
     
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        subSet(nums, 0, ans, list);
+        return ans;
+    }
 }

eadf49f1077355670da196d98d40691be7957c7f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-14 10:36:26 +0530

    Added Q698PartitionToKEqualSum code

--- Recursion/MediumQuestions/Q698PartitionToKEqualSum.java
+++ Recursion/MediumQuestions/Q698PartitionToKEqualSum.java
@@ -37,39 +37,25 @@ public class Q698PartitionToKEqualSum {
 
     public boolean canPartitionKSubsets(int a[], int target, int i, int bucket[]) {
 
-        // we have taken all the elements
         if (i == -1)
             return true;
 
-        // start filling the buckets
         for (int j = 0; j < bucket.length; j++) {
 
-            // can we take this ith element
             if (bucket[j] + a[i] <= target) {
 
                 // if we take this element
                 bucket[j] += a[i];
-
-                // go to next element (in our case go to smallest ele bcz we sorted)
-                // if we can fill all buckets then return true
                 if (canPartitionKSubsets(a, target, i - 1, bucket))
                     return true;
 
-                // means we can't fill other buckets if we take ith element so leave this
-                // element
                 bucket[j] -= a[i];
-
             }
-
-            // if our bucket is empty means we have not taken any elements in the buckets
             if (bucket[j] == 0)
                 break;
-
         }
 
-        // all buckets are full but i is pointing to some element (elements still left)
-        // or our bucket is empty means we haven't take any element (not valid)
+        //elements still left
         return false;
-
     }
 }

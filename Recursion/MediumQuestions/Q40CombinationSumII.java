2c823eb56006513b4010362b360d3c7e27d4672c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-14 09:40:56 +0530

    Added Q40CombinationSumII code

--- Recursion/MediumQuestions/Q40CombinationSumII.java
+++ Recursion/MediumQuestions/Q40CombinationSumII.java
@@ -1,5 +1,9 @@
 package Recursion.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Given a collection of candidate numbers (candidates) and a target number (target), find all unique
 combinations in candidates where the candidate numbers sum to target. Each number in candidates 
@@ -11,5 +15,12 @@ Example 1:
 */
 
 public class Q40CombinationSumII {
-    
+
+    private List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates); // Sort candidates array
+        solve(candidates, target, 0, new ArrayList<>()); // Call recursive function
+        return result;
+    }
 }

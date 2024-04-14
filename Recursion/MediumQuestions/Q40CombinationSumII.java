dd6451d4c0a8127814c0bfa59f502d8b6b41a335
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-14 09:41:12 +0530

    Added Q40CombinationSumII code

--- Recursion/MediumQuestions/Q40CombinationSumII.java
+++ Recursion/MediumQuestions/Q40CombinationSumII.java
@@ -23,4 +23,34 @@ public class Q40CombinationSumII {
         solve(candidates, target, 0, new ArrayList<>()); // Call recursive function
         return result;
     }
+
+    public void solve(int[] nums, int target, int index, List<Integer> list) {
+        // Base case: if target is achieved
+        if (target == 0) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j : list) {
+                temp.add(j);
+            }
+            result.add(temp); // Add current combination to result
+            return;
+        }
+
+        // Base case: if index exceeds array bounds or target is less than current
+        // element
+        if (index == nums.length || target < nums[index]) {
+            return;
+        }
+
+        int temp = nums[index];
+        list.add(nums[index]); // Choose current element
+        solve(nums, target - nums[index], index + 1, list); // Recur with reduced target and move to next index
+        list.remove(list.size() - 1); // Backtrack: remove last element
+
+        int i = 1;
+        while (index + i < nums.length && nums[index + i] == temp) {
+            i++; // Skip duplicates
+        }
+        solve(nums, target, index + i, list); // Recur without choosing current element
+        return;
+    }
 }

92387aac39820932ce459e7f12c3d802e118e36f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-14 10:33:17 +0530

    Added Q698PartitionToKEqualSum code

--- Recursion/MediumQuestions/Q698PartitionToKEqualSum.java
+++ Recursion/MediumQuestions/Q698PartitionToKEqualSum.java
@@ -1,5 +1,13 @@
 package Recursion.MediumQuestions;
 
+/*
+Given an integer array nums and an integer k, return true if it is possible to divide this array 
+into k non-empty subsets whose sums are all equal.
+
+Example 1:
+
+*/
+
 public class Q698PartitionToKEqualSum {
     
 }

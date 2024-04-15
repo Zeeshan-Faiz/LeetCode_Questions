ba1e95de66112d944f0b310af183e4f9b20570df
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-15 10:59:05 +0530

    Added Q18FourSum code

--- Arrays/MeduimQuestions/Q18FourSum.java
+++ Arrays/MeduimQuestions/Q18FourSum.java
@@ -1,5 +1,9 @@
 package Arrays.MeduimQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Given an array nums of n integers, return an array of all the unique quadruplets 
 [nums[a], nums[b], nums[c], nums[d]] such that:
@@ -18,5 +22,62 @@ Output: [[2,2,2,2]]
 */
 
 public class Q18FourSum {
-    
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums.length < 4) {
+            return ans;
+        }
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            
+            //avoid duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            
+            if ((long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; j++) {
+                //avoid duplicates
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                // 优化1
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                // 优化2
+                if ((long) nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) {
+                    continue;
+                }
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                    long sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum > target) {
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        // match
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 }

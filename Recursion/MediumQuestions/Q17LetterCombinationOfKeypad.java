bbfe9920dcd1eb79487eb20bef677250b6a6429f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-15 10:30:07 +0530

    Added Q17LetterCombinationOfKeypad code

--- Recursion/MediumQuestions/Q17LetterCombinationOfKeypad.java
+++ Recursion/MediumQuestions/Q17LetterCombinationOfKeypad.java
@@ -1,5 +1,8 @@
 package Recursion.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given a string containing digits from 2-9 inclusive, return all possible letter combinations that 
 the number could represent. Return the answer in any order.
@@ -21,4 +24,12 @@ Output: ["a","b","c"]
 
 public class Q17LetterCombinationOfKeypad {
     
+    List<String> res=new ArrayList<>();
+    String[] val= {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits) {
+        if(digits==null||digits.length()==0)return res;
+        getCombinations(0,new StringBuilder(),digits);
+        return res;
+
+    }
 }

110354ee602890c65e24956c8990ca40399cc3f0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-16 10:40:55 +0530

    Added Q287FindDuplicateNumber code

--- Arrays/MeduimQuestions/Q287FindDuplicateNumber.java
+++ Arrays/MeduimQuestions/Q287FindDuplicateNumber.java
@@ -1,11 +1,12 @@
 package Arrays.MeduimQuestions;
 
 /*
-Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-
-There is only one repeated number in nums, return this repeated number.
-
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n]
+inclusive. There is only one repeated number in nums, return this repeated number.
 You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
 */
 
 public class Q287FindDuplicateNumber {

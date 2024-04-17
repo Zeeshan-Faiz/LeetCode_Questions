c884aa03d1513d528f93fa842fbdb0a19a4c9fea
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-17 12:52:52 +0530

    Added Q55JumpGame code

--- Arrays/MeduimQuestions/Q55JumpGame.java
+++ Arrays/MeduimQuestions/Q55JumpGame.java
@@ -1,5 +1,14 @@
 package Arrays.MeduimQuestions;
 
+/*
+You are given an integer array nums. You are initially positioned at the array's first index, 
+and each element in the array represents your maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+
+*/
+
 public class Q55JumpGame {
     
 }

500a9fb1ccfc121417745db216f5d7b7ed409882
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-17 11:54:11 +0530

    Added Q442FindAllDuplicates code

--- Arrays/MeduimQuestions/Q442FindAllDuplicates.java
+++ Arrays/MeduimQuestions/Q442FindAllDuplicates.java
@@ -1,5 +1,8 @@
 package Arrays.MeduimQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given an integer array nums of length n where all the integers of nums are in the range [1, n] 
 and each integer appears once or twice, return an array of all the integers that appears twice.
@@ -19,5 +22,27 @@ Output: []
 */
 
 public class Q442FindAllDuplicates {
-    
+
+    public List<Integer> findDuplicates(int[] nums) {
+
+        // Use Cyclic sort and sort the array elements
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // find duplicate numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                ans.add(nums[index]);
+            }
+        }
+        return ans;
+    }
 }

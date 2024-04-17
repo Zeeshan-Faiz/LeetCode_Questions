597f2f645232836f9b6899aa4f4d88a7333cbe72
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-17 12:12:18 +0530

    Added Q532KDiffPairs code

--- Arrays/MeduimQuestions/Q532KDiffPairs.java
+++ Arrays/MeduimQuestions/Q532KDiffPairs.java
@@ -1,5 +1,7 @@
 package Arrays.MeduimQuestions;
 
+import java.util.Arrays;
+
 /*
 Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
 
@@ -28,4 +30,33 @@ Explanation: There is one 0-diff pair in the array, (1, 1).
 
 public class Q532KDiffPairs {
     
+    public int findPairs(int[] nums, int k) {
+
+        Arrays.sort(nums);
+        int count=0;
+        int n= nums.length;
+        int left = 0;
+        int right = 1;
+        while (left < n && right < n) 
+        {
+            if (left == right || nums[right] - nums[left] < k) 
+            {
+                right++;
+            } 
+            else if (nums[right] - nums[left] > k) 
+            {
+                left++;
+            } 
+            else 
+            {
+                count++;
+                left++;
+                while (left < nums.length && nums[left] == nums[left - 1]) 
+                {
+                    left++;
+                }
+            }
+        }
+        return count;
+    }
 }

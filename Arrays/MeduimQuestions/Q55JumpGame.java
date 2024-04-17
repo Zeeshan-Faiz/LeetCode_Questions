81095d801ba51cf0a77b67feaaac1d01055cc1d8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-17 12:54:02 +0530

    Added Q55JumpGame code

--- Arrays/MeduimQuestions/Q55JumpGame.java
+++ Arrays/MeduimQuestions/Q55JumpGame.java
@@ -17,14 +17,16 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum jump 
 */
 
 public class Q55JumpGame {
-    
+
     public boolean canJump(int[] nums) {
+        
         int end = nums.length - 1;
-        for(int i = end; i >= 0; i--){
-            if(i + nums[i] >= end){
+        for (int i = end; i >= 0; i--) 
+        {
+            if (i + nums[i] >= end) {
                 end = i;
             }
         }
-        return end == 0;     
+        return end == 0;
     }
 }

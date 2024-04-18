916386a214e4fff10a9ef2f5249037f4dd8a03e5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-18 14:27:01 +0530

    Added Q56MergeIntervals code

--- Arrays/MeduimQuestions/Q56MergeIntervals.java
+++ Arrays/MeduimQuestions/Q56MergeIntervals.java
@@ -1,5 +1,13 @@
 package Arrays.MeduimQuestions;
 
+/*
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
+and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+Example 1:
+
+*/
+
 public class Q56MergeIntervals {
     
 }

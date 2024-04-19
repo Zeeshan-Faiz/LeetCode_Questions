5c3f0d7f8539fd2b2eeb39aa43b1899faeefbdb7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-19 11:19:20 +0530

    Added Q1344AnglesBetweenHandsOfClock code

--- Maths/MediumQuestions/Q1344AnglesBetweenHandsOfClock.java
+++ Maths/MediumQuestions/Q1344AnglesBetweenHandsOfClock.java
@@ -1,5 +1,13 @@
 package Maths.MediumQuestions;
 
+/*
+Given two numbers, hour and minutes, return the smaller angle (in degrees) formed between the 
+hour and the minute hand. Answers within 10-5 of the actual value will be accepted as correct.
+
+Example 1:
+
+*/
+
 public class Q1344AnglesBetweenHandsOfClock {
     
 }

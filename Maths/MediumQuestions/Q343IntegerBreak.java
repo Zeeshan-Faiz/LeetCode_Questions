2c2f0e895127399bb0003802904f96a63c614605
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-19 11:49:55 +0530

    Added Q343IntegerBreak code

--- Maths/MediumQuestions/Q343IntegerBreak.java
+++ Maths/MediumQuestions/Q343IntegerBreak.java
@@ -1,5 +1,13 @@
 package Maths.MediumQuestions;
 
+/*
+Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the 
+product of those integers. Return the maximum product you can get.
+
+Example 1:
+
+*/
+
 public class Q343IntegerBreak {
     
 }

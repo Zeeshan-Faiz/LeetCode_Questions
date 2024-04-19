898987057be0fc133b005c35a307f6a91d8d5028
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-19 11:20:35 +0530

    Added Q1344AnglesBetweenHandsOfClock code

--- Maths/MediumQuestions/Q1344AnglesBetweenHandsOfClock.java
+++ Maths/MediumQuestions/Q1344AnglesBetweenHandsOfClock.java
@@ -18,16 +18,20 @@ Output: 7.5
 */
 
 public class Q1344AnglesBetweenHandsOfClock {
-    
+
     public double angleClock(int hour, int minutes) {
-        int totalMinutes=hour*60+minutes;
-       double hourAngle=totalMinutes*0.5;
-       double minuteAngle=minutes*6;
-       if(hourAngle>=360)
-           hourAngle=hourAngle-360;
-       double ans=Math.abs(minuteAngle-hourAngle);
-       if(ans>180.00)
-           ans=360.00-ans;
-       return ans;
-   }
+        
+        int totalMinutes = hour * 60 + minutes;
+        double hourAngle = totalMinutes * 0.5;
+        double minuteAngle = minutes * 6;
+        if (hourAngle >= 360)
+            hourAngle = hourAngle - 360;
+        
+        double ans = Math.abs(minuteAngle - hourAngle);
+        
+        if (ans > 180.00)
+            ans = 360.00 - ans;
+        
+        return ans;
+    }
 }

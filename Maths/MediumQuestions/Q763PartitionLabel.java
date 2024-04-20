6739fb92ed58de38d35fa81bd5e4c62750bc458f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-20 13:34:20 +0530

    Added Q763PartitionLabel code

--- Maths/MediumQuestions/Q763PartitionLabel.java
+++ Maths/MediumQuestions/Q763PartitionLabel.java
@@ -7,7 +7,16 @@ concatenating all the parts in order, the resultant string should be s.
 Return a list of integers representing the size of these parts.
 
 Example 1:
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
 
+Example 2:
+Input: s = "eccbbbbdec"
+Output: [10]
 */
 
 public class Q763PartitionLabel {

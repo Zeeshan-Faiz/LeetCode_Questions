3b9f1144838d342361e51e65246b9ada9c4a6b5b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-21 13:41:59 +0530

    Added Q1578MinimumTimeToMakeRopeColorful code

--- Greedy/MediumQuestions/Q1578MinimumTimeToMakeRopeColorful.java
+++ Greedy/MediumQuestions/Q1578MinimumTimeToMakeRopeColorful.java
@@ -7,6 +7,16 @@ consecutive balloons to be of the same color, so she asks Bob for help. Bob can 
 balloons from the rope to make it colorful. You are given a 0-indexed integer array neededTime 
 where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the 
 rope. Return the minimum time Bob needs to make the rope colorful.
+
+Example 1:
+Input: colors = "abaac", neededTime = [1,2,3,4,5]
+Output: 3
+Explanation: In the above image, 'a' is blue, 'b' is red, and 'c' is green.
+Bob can remove the blue balloon at index 2. This takes 3 seconds.
+There are no longer two consecutive balloons of the same color. Total time = 3.
+
+Example 2:
+
 */
 
 public class Q1578MinimumTimeToMakeRopeColorful {

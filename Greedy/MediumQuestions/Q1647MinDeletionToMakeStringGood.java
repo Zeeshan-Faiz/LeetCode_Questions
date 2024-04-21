26a0c2fe9eaeb673498ab9f2d8e6a9efd4e35992
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-21 14:01:50 +0530

    Added Q1647MinDeletionToMakeStringGood code

--- Greedy/MediumQuestions/Q1647MinDeletionToMakeStringGood.java
+++ Greedy/MediumQuestions/Q1647MinDeletionToMakeStringGood.java
@@ -1,5 +1,15 @@
 package Greedy.MediumQuestions;
 
+/*
+A string s is called good if there are no two different characters in s that have the same
+frequency. Given a string s, return the minimum number of characters you need to delete to make
+s good. The frequency of a character in a string is the number of times it appears in the string. 
+For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
+
+Example 1:
+
+*/
+
 public class Q1647MinDeletionToMakeStringGood {
     
 }

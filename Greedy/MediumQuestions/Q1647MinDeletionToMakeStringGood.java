3404667e315390085c9814ca640d1b0dddb2ba3d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-21 14:02:13 +0530

    Added Q1647MinDeletionToMakeStringGood code

--- Greedy/MediumQuestions/Q1647MinDeletionToMakeStringGood.java
+++ Greedy/MediumQuestions/Q1647MinDeletionToMakeStringGood.java
@@ -7,7 +7,21 @@ s good. The frequency of a character in a string is the number of times it appea
 For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
 
 Example 1:
+Input: s = "aab"
+Output: 0
+Explanation: s is already good.
 
+Example 2:
+Input: s = "aaabbbcc"
+Output: 2
+Explanation: You can delete two 'b's resulting in the good string "aaabcc".
+Another way it to delete one 'b' and one 'c' resulting in the good string "aaabbc".
+
+Example 3:
+Input: s = "ceabaacb"
+Output: 2
+Explanation: You can delete both 'c's resulting in the good string "eabaab".
+Note that we only care about characters that are still in the string at the end (i.e. frequency of 0 is ignored).
 */
 
 public class Q1647MinDeletionToMakeStringGood {

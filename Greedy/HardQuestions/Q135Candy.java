40ce053e0c9cdd853b2b1f3e7ae71e68aae44856
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-22 21:35:03 +0530

    Added Q135Candy code

--- Greedy/HardQuestions/Q135Candy.java
+++ Greedy/HardQuestions/Q135Candy.java
@@ -1,5 +1,7 @@
 package Greedy.HardQuestions;
 
+import java.util.Arrays;
+
 /*
 There are n children standing in a line. Each child is assigned a rating value given in the 
 integer array ratings.
@@ -23,4 +25,28 @@ The third child gets 1 candy because it satisfies the above two conditions.
 
 public class Q135Candy {
     
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
 }

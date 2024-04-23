3aad8e856e38d98cd5dc81aa1cbdd15274a53abd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-23 15:18:01 +0530

    Added Q394DecodeString code

--- Strings/MediumQuestions/Q394DecodeString.java
+++ Strings/MediumQuestions/Q394DecodeString.java
@@ -1,5 +1,7 @@
 package Strings.MediumQuestions;
 
+import java.util.Stack;
+
 /*
 Given an encoded string, return its decoded string.
 The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is 
@@ -26,4 +28,35 @@ Output: "abcabccdcdcdef"
 
 public class Q394DecodeString {
     
+    public String decodeString(String s) {
+        
+        Stack<Integer> repeatTimes = new Stack<>(); // this will store the current sb to repeat how many times
+        Stack<StringBuilder> substrs = new Stack<>();   // this will store the last created substring
+
+        char[] sarr = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int n = 0;
+
+        for(int i=0;i<sarr.length;i++) {
+            if(Character.isDigit(sarr[i])) {
+                n = n * 10 + (sarr[i] - '0');   // to conver char to number.
+            }else if(sarr[i] == '[') {
+                repeatTimes.push(n);    // Push the last found to repeatTimes
+                n = 0;  // reset for next n
+                substrs.push(sb);   // Store the current sb i.e. been made
+                sb = new StringBuilder();   // create new sb for reading the characters after current [
+            }else if(sarr[i] == ']') {  // if reached ] then sb will have the value between [...]
+                int k = repeatTimes.pop(); // no of times to repeat the current sb
+                StringBuilder temp = sb;    // copy the current sb to temp
+                sb = substrs.pop(); // pop the last created substring
+
+                while(k-->0) {  // append the current substring to sb. So after this as it will get [ it will append the constructed substring again to stack to which we will again append the next encoding.
+                    sb.append(temp);
+                }
+            }else {
+                sb.append(sarr[i]);
+            }
+        }
+        return sb.toString();
+    }
 }

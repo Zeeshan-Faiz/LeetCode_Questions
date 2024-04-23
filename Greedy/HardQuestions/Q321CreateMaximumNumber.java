58cf6bbd0bc96c5504c47579ea77c5ee3f86fa68
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-23 14:28:39 +0530

    Added Q321CreateMaximumNumber code

--- Greedy/HardQuestions/Q321CreateMaximumNumber.java
+++ Greedy/HardQuestions/Q321CreateMaximumNumber.java
@@ -32,4 +32,19 @@ public class Q321CreateMaximumNumber {
         }
         return ans;
     }
+
+    private int[] merge(int[] nums1, int[] nums2, int k) {
+        int[] ans = new int[k];
+        for (int i = 0, j = 0, r = 0; r < k; ++r)
+            ans[r] = greater(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
+        return ans;
+    }
+
+    public boolean greater(int[] nums1, int i, int[] nums2, int j) {
+        while (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
+            i++;
+            j++;
+        }
+        return j == nums2.length || (i < nums1.length && nums1[i] > nums2[j]);
+    }
 }

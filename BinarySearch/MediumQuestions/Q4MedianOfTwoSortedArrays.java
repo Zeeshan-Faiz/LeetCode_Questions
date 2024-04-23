97b951940e96c2feb3501d26f59129692e8a8506
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-23 16:44:00 +0530

    Added Q4MedianOfTwoSortedArrays code

--- BinarySearch/MediumQuestions/Q4MedianOfTwoSortedArrays.java
+++ BinarySearch/MediumQuestions/Q4MedianOfTwoSortedArrays.java
@@ -1,5 +1,13 @@
 package BinarySearch.MediumQuestions;
 
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the 
+two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+
+*/
+
 public class Q4MedianOfTwoSortedArrays {
     
 }

c67d8aa2e9f55b44081833786b4ca392bffc6845
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-24 18:42:27 +0530

    Added Q75SortColors code

--- BinarySearch/MediumQuestions/Q75SortColors.java
+++ BinarySearch/MediumQuestions/Q75SortColors.java
@@ -7,8 +7,12 @@ integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
 You must solve this problem without using the library's sort function.
 
 Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
 
-
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
 */
 
 public class Q75SortColors {

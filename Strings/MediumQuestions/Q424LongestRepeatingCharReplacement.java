283d72fb522f5c44387e687586dc9694e4d88ec2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-24 18:57:11 +0530

    Added Q424LongestRepeatingCharReplacement code

--- Strings/MediumQuestions/Q424LongestRepeatingCharReplacement.java
+++ Strings/MediumQuestions/Q424LongestRepeatingCharReplacement.java
@@ -21,4 +21,19 @@ There may exists other ways to achieve this answer too.
 
 public class Q424LongestRepeatingCharReplacement {
     
+    public int characterReplacement(String s, int k) {
+        int[] arr = new int[26];
+        char[] ans = s.toCharArray();
+        int left = 0;
+        int max = 0;
+        for (int right = 0; right < ans.length; right++) {
+            arr[ans[right] - 'A']++;
+            max = Math.max(max, arr[ans[right] - 'A']);
+
+            while ((right - left + 1) - max > k) {
+                arr[ans[left++] - 'A']--;
+            }
+        }
+        return ans.length - left;
+    }
 }

de9b1d15633ebb94f9509140bb361bdf940e5364
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-25 19:16:18 +0530

    Added Q992SubarrayWithKDiffIntegers code

--- Arrays/HardQuestions/Q992SubarrayWithKDiffIntegers.java
+++ Arrays/HardQuestions/Q992SubarrayWithKDiffIntegers.java
@@ -1,5 +1,15 @@
 package Arrays.HardQuestions;
 
+/*
+Given an integer array nums and an integer k, return the number of good subarrays of nums.
+A good array is an array where the number of different integers in that array is exactly k.
+    For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+A subarray is a contiguous part of an array.
+
+Example 1:
+
+*/
+
 public class Q992SubarrayWithKDiffIntegers {
     
 }

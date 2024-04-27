0bf998a11edccd5dc9e1eb942355fbb7cc744440
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-27 12:00:47 +0530

    Added Q2149RearrangeElementsBySign code

--- Arrays/MeduimQuestions/Q2149RearrangeElementsBySign.java
+++ Arrays/MeduimQuestions/Q2149RearrangeElementsBySign.java
@@ -26,19 +26,20 @@ So nums is rearranged to [1,-1].
 */
 
 public class Q2149RearrangeElementsBySign {
-    
+
     public int[] rearrangeArray(int[] nums) {
-        int pos=0;
-        int neg=1;
-        int ans[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-          if(nums[i]<0){
-            ans[neg]=nums[i];
-            neg+=2;
-          }else{
-            ans[pos]=nums[i];
-            pos+=2;
-          }
+
+        int pos = 0, neg = 1;
+        int ans[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                ans[neg] = nums[i];
+                neg += 2;
+            } 
+            else {
+                ans[pos] = nums[i];
+                pos += 2;
+            }
         }
         return ans;
     }

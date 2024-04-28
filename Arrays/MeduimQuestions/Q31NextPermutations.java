cdd00e4cbf5a8eaaba7fd9862a890c8e2ad88186
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-28 14:44:46 +0530

    Added Q31NextPermutations code

--- Arrays/MeduimQuestions/Q31NextPermutations.java
+++ Arrays/MeduimQuestions/Q31NextPermutations.java
@@ -17,8 +17,21 @@ the array must be rearranged as the lowest possible order (i.e., sorted in ascen
 
 Given an array of integers nums, find the next permutation of nums. The replacement must be in 
 place and use only constant extra memory.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: nums = [3,2,1]
+Output: [1,2,3]
+
+Example 3:
+Input: nums = [1,1,5]
+Output: [1,5,1]
 */
 
 public class Q31NextPermutations {
+    
     
 }

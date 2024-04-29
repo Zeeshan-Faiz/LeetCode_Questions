b0cefb653a9405afc31baca64caf89491574c1f4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-04-29 14:00:03 +0530

    Added Q73SetMatrixZeroes code

--- Arrays/MeduimQuestions/Q73SetMatrixZeroes.java
+++ Arrays/MeduimQuestions/Q73SetMatrixZeroes.java
@@ -15,4 +15,29 @@ Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 
 public class Q73SetMatrixZeroes {
     
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rowZero = new boolean[m];
+        boolean[] colZero = new boolean[n];
+        
+        // Mark rows and columns containing zeroes
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rowZero[i] = true;
+                    colZero[j] = true;
+                }
+            }
+        }
+        
+        // Set zeroes based on rowZero and colZero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rowZero[i] || colZero[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }

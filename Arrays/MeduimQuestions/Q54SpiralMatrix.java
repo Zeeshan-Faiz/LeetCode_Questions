fbd31d9697cd3bc4fc5287623c168fbe08ad8c4d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-01 18:53:40 +0530

    Added Q54SpiralMatrix code

--- Arrays/MeduimQuestions/Q54SpiralMatrix.java
+++ Arrays/MeduimQuestions/Q54SpiralMatrix.java
@@ -16,34 +16,36 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 */
 
 public class Q54SpiralMatrix {
-    
+
     public List<Integer> spiralOrder(int[][] matrix) {
-         List<Integer> result = new ArrayList<>();
+        
+        List<Integer> result = new ArrayList<>();
         if (matrix == null || matrix.length == 0) {
             return result;
         }
-        
+
         int rows = matrix.length, cols = matrix[0].length;
-        int left = 0, right = cols-1, top = 0, bottom = rows-1;
-        
-        while (left <= right && top <= bottom) {
+        int left = 0, right = cols - 1, top = 0, bottom = rows - 1;
+
+        while (left <= right && top <= bottom) 
+        {
             for (int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
             top++;
-            
+
             for (int i = top; i <= bottom; i++) {
                 result.add(matrix[i][right]);
             }
             right--;
-            
+
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
                     result.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
-            
+
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     result.add(matrix[i][left]);
@@ -51,7 +53,6 @@ public class Q54SpiralMatrix {
                 left++;
             }
         }
-        
         return result;
     }
 }

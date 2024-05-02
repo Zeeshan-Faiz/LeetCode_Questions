a6500e599a955bdf3cf5ac8f4d36b8d8986732f7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-02 18:14:21 +0530

    Added Q19RemoveNthNodeFromEndOfLL code

--- LinkedList/MeduimQuestions/Q19RemoveNthNodeFromEndOfLL.java
+++ LinkedList/MeduimQuestions/Q19RemoveNthNodeFromEndOfLL.java
@@ -13,22 +13,24 @@ Output: [1]
 */
 
 public class Q19RemoveNthNodeFromEndOfLL {
-    
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        //create a new node with val = 0 and pointing to head
         ListNode res = new ListNode(0, head);
-       ListNode dummy = res;
+        ListNode dummy = res;
 
-       for (int i = 0; i < n; i++) {
-           head = head.next;
-       }
+        for (int i = 0; i < n; i++) {
+            head = head.next;
+        }
 
-       while (head != null) {
-           head = head.next;
-           dummy = dummy.next;
-       }
+        while (head != null) {
+            head = head.next;
+            dummy = dummy.next;
+        }
 
-       dummy.next = dummy.next.next;
+        dummy.next = dummy.next.next;
 
-       return res.next;        
-   }
+        return res.next;
+    }
 }

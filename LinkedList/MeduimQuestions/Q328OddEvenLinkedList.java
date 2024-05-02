0e08ebee352acf144762a951b637b0667465ff08
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-02 17:57:47 +0530

    Added Q328OddEvenLinkedList code

--- LinkedList/MeduimQuestions/Q328OddEvenLinkedList.java
+++ LinkedList/MeduimQuestions/Q328OddEvenLinkedList.java
@@ -7,7 +7,12 @@ the second node is even, and so on. Note that the relative order inside both the
 should remain as it was in the input. You must solve the problem in O(1) extra space complexity and O(n) time complexity.
 
 Example 1:
+Input: head = [1,2,3,4,5]
+Output: [1,3,5,2,4]
 
+Example 2:
+Input: head = [2,1,3,5,6,4,7]
+Output: [2,3,6,7,1,5,4]
 */
 
 public class Q328OddEvenLinkedList {

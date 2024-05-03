d2e9a7ac61e5740ec7e3a963df3fe74801f3f9f4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-03 17:34:10 +0530

    Added Q2095DeleteMiddleNodeOfLL code

--- LinkedList/MeduimQuestions/Q2095DeleteMiddleNodeOfLL.java
+++ LinkedList/MeduimQuestions/Q2095DeleteMiddleNodeOfLL.java
@@ -24,4 +24,23 @@ For n = 4, node 2 with value 3 is the middle node, which is marked in red.
 
 public class Q2095DeleteMiddleNodeOfLL {
     
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next==null){
+            return null;
+        }
+        if (head.next.next==null){ 
+            head.next=null;           
+            return head;
+        }
+
+        ListNode fast = head.next.next; 
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
 }

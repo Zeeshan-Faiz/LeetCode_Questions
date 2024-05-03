621fc64ae2f0a0370dc8c018bf0895e0d678b435
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-03 17:46:19 +0530

    Added Q2AddTwoNumbers code

--- LinkedList/MeduimQuestions/Q2AddTwoNumbers.java
+++ LinkedList/MeduimQuestions/Q2AddTwoNumbers.java
@@ -1,5 +1,15 @@
 package LinkedList.MeduimQuestions;
 
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are 
+stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and 
+return the sum as a linked list. You may assume the two numbers do not contain any leading zero, 
+except the number 0 itself.
+
+Example 1:
+
+*/
+
 public class Q2AddTwoNumbers {
     
 }

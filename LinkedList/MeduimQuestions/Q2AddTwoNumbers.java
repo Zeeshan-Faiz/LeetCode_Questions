fe9e9e9ae835945cf5aa8b12f335ce66c6140445
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-03 17:47:20 +0530

    Added Q2AddTwoNumbers code

--- LinkedList/MeduimQuestions/Q2AddTwoNumbers.java
+++ LinkedList/MeduimQuestions/Q2AddTwoNumbers.java
@@ -21,15 +21,16 @@ Output: [8,9,9,9,0,0,0,1]
 */
 
 public class Q2AddTwoNumbers {
-    
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
         ListNode dummy = new ListNode();
         ListNode res = dummy;
         int total = 0, carry = 0;
 
         while (l1 != null || l2 != null || carry != 0) {
+            
             total = carry;
-
             if (l1 != null) {
                 total += l1.val;
                 l1 = l1.next;
@@ -44,7 +45,6 @@ public class Q2AddTwoNumbers {
             dummy.next = new ListNode(num);
             dummy = dummy.next;
         }
-
-        return res.next;    
+        return res.next;
     }
 }

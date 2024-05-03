6fb592f8c05db82504815dc5f1805f5a665f946b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-03 17:33:56 +0530

    Added Q2095DeleteMiddleNodeOfLL code

--- LinkedList/MeduimQuestions/Q2095DeleteMiddleNodeOfLL.java
+++ LinkedList/MeduimQuestions/Q2095DeleteMiddleNodeOfLL.java
@@ -7,7 +7,19 @@ start using 0-based indexing, where ⌊x⌋ denotes the largest integer less tha
     For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
 
 Example 1:
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node. 
 
+Example 2:
+Input: head = [1,2,3,4]
+Output: [1,2,4]
+Explanation:
+The above figure represents the given linked list.
+For n = 4, node 2 with value 3 is the middle node, which is marked in red.
 */
 
 public class Q2095DeleteMiddleNodeOfLL {

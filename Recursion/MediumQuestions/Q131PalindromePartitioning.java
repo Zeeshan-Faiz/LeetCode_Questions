e76826ae13991cd0ede7b50627422b8804345c6b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-06 11:44:29 +0530

    Added Q131PalindromePartitioning code

--- Recursion/MediumQuestions/Q131PalindromePartitioning.java
+++ Recursion/MediumQuestions/Q131PalindromePartitioning.java
@@ -1,5 +1,8 @@
 package Recursion.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given a string s, partition s such that every substring of the partition is a palindrome. Return 
 all possible palindrome partitioning of s.
@@ -15,8 +18,9 @@ Output: [["a"]]
 */
 
 public class Q131PalindromePartitioning {
-    
+
     public List<List<String>> partition(String s) {
+        
         List<List<String>> result = new ArrayList<>();
         if (s == null || s.length() == 0) {
             return result;
@@ -26,5 +30,28 @@ public class Q131PalindromePartitioning {
         return result;
     }
 
-    
+    private void helper(String s, int start, List<String> list, List<List<String>> result) {
+        
+        if (start == s.length()) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome(s, start, i)) {
+                list.add(s.substring(start, i + 1));
+                helper(s, i + 1, list, result);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }

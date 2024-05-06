fd14f9abad2e7e899e8aa6916920f54994f763e3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-06 12:13:16 +0530

    Added Q2104SumOfSubarrayRanges code

--- StackandQueue/MediumQuestions/Q2104SumOfSubarrayRanges.java
+++ StackandQueue/MediumQuestions/Q2104SumOfSubarrayRanges.java
@@ -1,5 +1,14 @@
 package StackandQueue.MediumQuestions;
 
+/*
+You are given an integer array nums. The range of a subarray of nums is the difference between the 
+largest and smallest element in the subarray. Return the sum of all subarray ranges of nums.
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+
+*/
+
 public class Q2104SumOfSubarrayRanges {
     
 }

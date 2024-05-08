61c984aa834024cb94b1de05ffb9d70f3f115ffb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-08 13:24:36 +0530

    Added Q450DeleteNodeInBST code

--- Tree/MediumQuestions/Q450DeleteNodeInBST.java
+++ Tree/MediumQuestions/Q450DeleteNodeInBST.java
@@ -23,4 +23,42 @@ Explanation: The tree does not contain a node with value = 0.
 
 public class Q450DeleteNodeInBST {
     
+    public TreeNode deleteNode(TreeNode root, int key) {
+        
+        if(root == null ) {
+            return null;
+        }
+        if(root.val<key){
+            root.right = deleteNode(root.right,key);
+        }
+        else if(root.val>key) {
+            root.left = deleteNode(root.left,key);
+        }
+        else{
+            //case 1: leaf node
+            if(root.left == null && root.right == null ) {
+                return null;
+            }
+            //case 2 : one child
+            if(root.left ==  null){
+                return root.right;
+            }
+            else if(root.right ==  null ) {
+                return root.left;
+            }
+            //case 3 : two children
+            TreeNode IS = findInOrderSuccessor(root.right);
+            root.val = IS.val;
+            
+            root.right = deleteNode(root.right,IS.val);
+        }
+        return root;
+
+    }
+    private TreeNode findInOrderSuccessor(TreeNode root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
 }

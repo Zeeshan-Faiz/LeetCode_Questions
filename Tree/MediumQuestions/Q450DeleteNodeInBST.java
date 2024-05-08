75c5e61d221f6c37faa246de5081f0a2b8c1cbb5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-08 13:24:22 +0530

    Added Q450DeleteNodeInBST code

--- Tree/MediumQuestions/Q450DeleteNodeInBST.java
+++ Tree/MediumQuestions/Q450DeleteNodeInBST.java
@@ -9,8 +9,16 @@ Basically, the deletion can be divided into two stages:
     If the node is found, delete the node.
 
 Example 1:
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
 
-
+Example 2:
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
 */
 
 public class Q450DeleteNodeInBST {

375189875a055dd8b03cd4d6d88aad5b4fc42552
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-09 11:37:38 +0530

    Added Q700SearchInBST code

--- Tree/EasyQuestions/Q700SearchInBST.java
+++ Tree/EasyQuestions/Q700SearchInBST.java
@@ -15,11 +15,29 @@ Output: []
 */
 
 public class Q700SearchInBST {
-    
+
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) return null;
-        if (val > root.val) return searchBST(root.right, val);
-        else if (val < root.val) return searchBST(root.left, val);
-        else return root;
+        
+        if (root == null)
+            return null;
+        if (val > root.val)
+            return searchBST(root.right, val);
+        else if (val < root.val)
+            return searchBST(root.left, val);
+        else
+            return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

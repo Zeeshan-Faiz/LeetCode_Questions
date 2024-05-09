3378fabf8945d6cece678a55bdd962735686eabf
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-09 11:21:25 +0530

    Added Q701InsertIntoBSTcode

--- Tree/MediumQuestions/Q701InsertIntoBST.java
+++ Tree/MediumQuestions/Q701InsertIntoBST.java
@@ -7,7 +7,12 @@ exist in the original BST. Notice that there may exist multiple valid ways for t
 long as the tree remains a BST after insertion. You can return any of them.
 
 Example 1:
+Input: root = [4,2,7,1,3], val = 5
+Output: [4,2,7,1,3,5]
 
+Example 2:
+Input: root = [40,20,60,10,30,50,70], val = 25
+Output: [40,20,60,10,30,50,70,null,null,25]
 */
 
 public class Q701InsertIntoBST {

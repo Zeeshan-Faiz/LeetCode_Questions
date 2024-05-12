2c8432baf82860ac3e216e79b8e500cc0a331bae
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-12 10:53:04 +0530

    Added Q214ShortestPalindrome code

--- Strings/MediumQuestions/Q214ShortestPalindrome.java
+++ Strings/MediumQuestions/Q214ShortestPalindrome.java
@@ -16,10 +16,29 @@ Output: "dcbabcd"
 public class Q214ShortestPalindrome {
 
     public String shortestPalindrome(String s) {
-        
+
         char[] arr = s.toCharArray();
         StringBuilder sb = new StringBuilder();
         helper(arr, arr.length, sb);
         return sb.toString();
+    }
+
+    private void helper(char[] arr, int n, StringBuilder sb) {
+        
+        int i = 0;
+        for (int j = n - 1; j >= 0; j--) {
+            if (arr[j] == arr[i]) {
+                i++;
+            }
+        }
+        if (i == n) {
+            sb.append(arr, 0, n);
+            return;
+        }
+        for (int j = n - 1; j >= i; j--) {
+            sb.append(arr[j]);
+        }
+        helper(arr, i, sb);
+        sb.append(arr, i, n - i);
     }
 }

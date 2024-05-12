f1b58e559ce4c5527c5e20622901c49ef870fd43
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-12 10:52:12 +0530

    Added Q214ShortestPalindrome code

--- Strings/MediumQuestions/Q214ShortestPalindrome.java
+++ Strings/MediumQuestions/Q214ShortestPalindrome.java
@@ -5,7 +5,12 @@ You are given a string s. You can convert s to a palindrome by adding characters
 Return the shortest palindrome you can find by performing this transformation.
 
 Example 1:
+Input: s = "aacecaaa"
+Output: "aaacecaaa"
 
+Example 2:
+Input: s = "abcd"
+Output: "dcbabcd"
 */
 
 public class Q214ShortestPalindrome {

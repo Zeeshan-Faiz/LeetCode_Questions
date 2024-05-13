2a5eaf8356c1b4b168bce8ce4cc7f01203b973fc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-13 21:58:21 +0530

    Added Q1004MaxConsecOnesIII code

--- TwoPointer/MeduimQuestions/Q1004MaxConsecOnesIII.java
+++ TwoPointer/MeduimQuestions/Q1004MaxConsecOnesIII.java
@@ -1,5 +1,12 @@
 package TwoPointer.MeduimQuestions;
 
+/*
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Example 1:
+
+*/
+
 public class Q1004MaxConsecOnesIII {
     
 }

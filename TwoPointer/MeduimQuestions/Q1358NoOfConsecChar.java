4188f16f6dedb20401e98f5ae31eb23af8a47851
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-14 10:06:07 +0530

    Added Q1358NoOfConsecChar code

--- TwoPointer/MeduimQuestions/Q1358NoOfConsecChar.java
+++ TwoPointer/MeduimQuestions/Q1358NoOfConsecChar.java
@@ -22,4 +22,27 @@ Output: 1
 
 public class Q1358NoOfConsecChar {
     
+    public int numberOfSubstrings(String s) {
+        
+        int l = 0, r = 0, n = s.length();
+        int ans = 0, hash[] = new int[3], count=0;
+        char[] arr = s.toCharArray();
+        while(r<n) {
+            char ch = arr[r];
+            hash[ch-'a']++;
+            if(hash[ch-'a']==1) count++;
+            while (count == 3) {
+                ans += n - r;
+                char rem = arr[l];
+                int val = hash[rem-'a'];
+                if(val==1) {
+                    count--;
+                }
+                hash[rem-'a']--;
+                l++;
+            } 
+            r++;
+        }
+        return ans;
+    }
 }

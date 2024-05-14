d100a4b74e216a1d9d7fface0b90699828892864
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-14 10:17:00 +0530

    Added Q76MinWindowSubstring code

--- TwoPointer/HardQuestions/Q76MinWindowSubstring.java
+++ TwoPointer/HardQuestions/Q76MinWindowSubstring.java
@@ -1,5 +1,14 @@
 package TwoPointer.HardQuestions;
 
+/*
+Given two strings s and t of lengths m and n respectively, return the minimum window
+substring of s such that every character in t (including duplicates) is included in the window. 
+If there is no such substring, return the empty string "". The testcases will be generated such that the answer is unique.
+
+Example 1:
+
+*/
+
 public class Q76MinWindowSubstring {
     
 }

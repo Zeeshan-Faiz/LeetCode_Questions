e8ecc6fd5307780fb2a158719b4fa4ea407e3e62
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-14 10:17:22 +0530

    Added Q76MinWindowSubstring code

--- TwoPointer/HardQuestions/Q76MinWindowSubstring.java
+++ TwoPointer/HardQuestions/Q76MinWindowSubstring.java
@@ -24,4 +24,25 @@ Since the largest window of s only has one 'a', return empty string.
 
 public class Q76MinWindowSubstring {
     
+    public String minWindow(String s, String t) {
+        int[] map = new int[128];
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
+        int counter = t.length(), begin = 0, end = 0, d = Integer.MAX_VALUE, head = 0;
+        while (end < s.length()) {
+            if (map[s.charAt(end++)]-- > 0) { // in t
+                counter--;
+            }
+            while (counter == 0) { // valid
+                if (end - begin < d) {
+                    d = end - (head = begin);
+                }
+                if (map[s.charAt(begin++)]++ == 0) { // make it invalid
+                    counter++;
+                }
+            }
+        }
+        return d == Integer.MAX_VALUE ? "" : s.substring(head, head + d);
+    }
 }

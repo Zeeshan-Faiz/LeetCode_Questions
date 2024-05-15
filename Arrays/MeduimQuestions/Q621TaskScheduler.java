b5d3944b9b513dbe2deaa9e7ddf44b42d40ad0aa
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-15 10:46:19 +0530

    Added Q621TaskScheduler code

--- Arrays/MeduimQuestions/Q621TaskScheduler.java
+++ Arrays/MeduimQuestions/Q621TaskScheduler.java
@@ -33,30 +33,23 @@ This leads to idling twice between repetitions of these tasks.
 public class Q621TaskScheduler {
     
     public int leastInterval(char[] tasks, int n) {
+        
         int[] frequency = new int[26];
         
-        // Count the frequency of each task
         for (char task : tasks) {
             frequency[task - 'A']++;
         }
         
-        // Sort the frequencies in ascending order
         Arrays.sort(frequency);
-        
-        // Find the maximum frequency (subtract 1 to account for the last task)
         int maxFreq = frequency[25] - 1;
         
         // Calculate the total idle slots needed
         int idleSlots = maxFreq * n;
         
-        // Iterate through the frequencies from the second highest to the lowest
         for (int i = 24; i >= 0 && frequency[i] > 0; i--) {
             // Subtract the minimum of maxFreq and current frequency from idle slots
             idleSlots -= Math.min(maxFreq, frequency[i]);
         }
-        
-        // If idle slots are still remaining, add them to the total length
-        // Otherwise, return the original length of tasks array
         return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
     }
 }

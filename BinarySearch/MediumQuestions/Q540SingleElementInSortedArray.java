d150012b52f8d79cea908758d7148c5d78395b6a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-17 09:34:20 +0530

    Added Q540SingleElementInSortedArray code

--- BinarySearch/MediumQuestions/Q540SingleElementInSortedArray.java
+++ BinarySearch/MediumQuestions/Q540SingleElementInSortedArray.java
@@ -15,5 +15,22 @@ Output: 10
 */
 
 public class Q540SingleElementInSortedArray {
-    
+
+    public int singleNonDuplicate(int[] nums) {
+
+        int l = 0, r = nums.length - 1;
+
+        while (l < r) {
+
+            int mid = l + (r - l) / 2;
+            if (mid % 2 == 1)
+                mid--;
+
+            if (nums[mid] != nums[mid + 1]) 
+                r = mid;
+            else 
+                l = mid + 2;
+        }
+        return nums[l];
+    }
 }

b34b3cd2599736d7e149def61ede450dae3f4ac2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-18 16:46:50 +0530

    Added Q74Searcha2DMatrix code

--- Arrays/MeduimQuestions/Q74Searcha2DMatrix.java
+++ Arrays/MeduimQuestions/Q74Searcha2DMatrix.java
@@ -1,5 +1,16 @@
 package Arrays.MeduimQuestions;
 
+/*
+You are given an m x n integer matrix matrix with the following two properties:
+    Each row is sorted in non-decreasing order.
+    The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+You must write a solution in O(log(m * n)) time complexity.
+
+Example 1:
+
+*/
+
 public class Q74Searcha2DMatrix {
     
 }

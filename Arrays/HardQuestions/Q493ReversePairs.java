d4fa0bee7bacde234151f450858307b7c6fc698f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-18 16:40:06 +0530

    Added Q493ReversePairs code

--- Arrays/HardQuestions/Q493ReversePairs.java
+++ Arrays/HardQuestions/Q493ReversePairs.java
@@ -8,7 +8,19 @@ A reverse pair is a pair (i, j) where:
     nums[i] > 2 * nums[j].
 
 Example 1:
+Input: nums = [1,3,2,3,1]
+Output: 2
+Explanation: The reverse pairs are:
+(1, 4) --> nums[1] = 3, nums[4] = 1, 3 > 2 * 1
+(3, 4) --> nums[3] = 3, nums[4] = 1, 3 > 2 * 1
 
+Example 2:
+Input: nums = [2,4,3,5,1]
+Output: 3
+Explanation: The reverse pairs are:
+(1, 4) --> nums[1] = 4, nums[4] = 1, 4 > 2 * 1
+(2, 4) --> nums[2] = 3, nums[4] = 1, 3 > 2 * 1
+(3, 4) --> nums[3] = 5, nums[4] = 1, 5 > 2 * 1
 */
 
 public class Q493ReversePairs {

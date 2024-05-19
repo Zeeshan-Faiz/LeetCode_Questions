f1e35aa5d008a34103528f24f85f72c49cffb63e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-19 16:49:58 +0530

    Added Q60PermutationSequence code

--- Arrays/HardQuestions/Q60PermutationSequence.java
+++ Arrays/HardQuestions/Q60PermutationSequence.java
@@ -1,5 +1,7 @@
 package Arrays.HardQuestions;
 
+import java.util.ArrayList;
+
 /*
 The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
 
@@ -27,4 +29,20 @@ Output: "123"
 
 public class Q60PermutationSequence {
     
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> num = new ArrayList<>();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+            num.add(i);
+        }
+        for (int i = 0, l = k - 1; i < n; i++) {
+            fact /= (n - i);
+            int index = (l / fact);
+            sb.append(num.remove(index));
+            l -= index * fact;
+        }
+        return sb.toString();
+    }
 }

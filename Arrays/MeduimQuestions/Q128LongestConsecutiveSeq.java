f1da7a31fbcf144a287ae672fdaa0b40460c8fcf
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-19 15:20:25 +0530

    Added Q128LongestConsecutiveSeq code

--- Arrays/MeduimQuestions/Q128LongestConsecutiveSeq.java
+++ Arrays/MeduimQuestions/Q128LongestConsecutiveSeq.java
@@ -1,5 +1,10 @@
 package Arrays.MeduimQuestions;
 
+/*
+Given an unsorted array of integers nums, return the length of the longest consecutive elements 
+sequence. You must write an algorithm that runs in O(n) time.
+*/
+
 public class Q128LongestConsecutiveSeq{
 
 }

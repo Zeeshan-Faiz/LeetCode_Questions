0a643ced0e38ef73ebe85e0603233b97ceda868a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-19 15:29:56 +0530

    Added Q485MaxConsecutive1s code

--- Arrays/EasyQuestions/Q485MaxConsecutive1s.java
+++ Arrays/EasyQuestions/Q485MaxConsecutive1s.java
@@ -1,5 +1,12 @@
 package Arrays.EasyQuestions;
 
+/*
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+
+*/
+
 public class Q485MaxConsecutive1s {
     
 }

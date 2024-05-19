18617f0e3b137abf4bdb083e63b3f17f916684ca
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-19 15:30:17 +0530

    Added Q485MaxConsecutive1s code

--- Arrays/EasyQuestions/Q485MaxConsecutive1s.java
+++ Arrays/EasyQuestions/Q485MaxConsecutive1s.java
@@ -15,4 +15,17 @@ Output: 2
 
 public class Q485MaxConsecutive1s {
     
+    public int findMaxConsecutiveOnes(int[] nums) {
+       int maxCount = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+            } else {
+                maxCount = Math.max(count, maxCount);
+                count = 0;
+            }
+        }
+        return Math.max(count, maxCount);
+    }
 }

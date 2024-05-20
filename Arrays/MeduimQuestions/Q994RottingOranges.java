6f9222588d91d5ab7dc07871b8c2131ee78605cc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-20 10:10:34 +0530

    Added Q994RottingOranges code

--- Arrays/MeduimQuestions/Q994RottingOranges.java
+++ Arrays/MeduimQuestions/Q994RottingOranges.java
@@ -29,31 +29,34 @@ Explanation: Since there are already no fresh oranges at minute 0, the answer is
 */
 
 public class Q994RottingOranges {
-    
+
     public int orangesRotting(int[][] grid) {
+        
         int m = grid.length;
         int n = grid[0].length;
         int[][] visited = grid;
         Queue<int[]> q = new LinkedList<>();
         int countFreshOrange = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 2) {
-                    q.offer(new int[] {i, j});
-                }
-                if (visited[i][j] == 1) {
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                if (visited[i][j] == 2)
+                    q.offer(new int[] { i, j });
+            
+                if (visited[i][j] == 1)
                     countFreshOrange++;
-                }
             }
         }
         if (countFreshOrange == 0)
             return 0;
         if (q.isEmpty())
             return -1;
-        
+
         int minutes = -1;
-        int[][] dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
-        while (!q.isEmpty()) {
+        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+        while (!q.isEmpty()) 
+        {
             int size = q.size();
             while (size-- > 0) {
                 int[] cell = q.poll();
@@ -65,15 +68,15 @@ public class Q994RottingOranges {
                     if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
                         visited[i][j] = 2;
                         countFreshOrange--;
-                        q.offer(new int[] {i, j});
+                        q.offer(new int[] { i, j });
                     }
                 }
             }
             minutes++;
         }
-        
         if (countFreshOrange == 0)
             return minutes;
+        
         return -1;
     }
 }

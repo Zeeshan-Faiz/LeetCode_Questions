cf7575c84fe2e93132548f4c4f8f2328a55da3ce
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-21 09:59:15 +0530

    Added Q165CompareVersionNumber code

--- Strings/MediumQuestions/Q165CompareVersionNumber.java
+++ Strings/MediumQuestions/Q165CompareVersionNumber.java
@@ -30,4 +30,26 @@ Explanation: version1 has less revisions, which means every missing revision are
 
 public class Q165CompareVersionNumber {
     
+    public int compareVersion(String v1, String v2) {
+        int i = 0, j = 0;
+        while(i < v1.length() || j < v2.length()) {
+            int n1 = 0, n2 = 0;
+            while(i < v1.length() && v1.charAt(i) != '.') {
+                n1 = n1*10 + (int)(v1.charAt(i) - '0');
+                ++i;
+            }
+            while(j < v2.length() && v2.charAt(j) != '.') {
+                n2 = n2*10 + (int)(v2.charAt(j) - '0');
+                ++j;
+            }
+
+            if(n1 > n2) return 1;
+            if(n1 < n2) return -1;
+
+            ++i;
+            ++j;
+        }
+
+        return 0;
+    }
 }

c247ba4461222422598fb0572dec967e38158f38
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-21 09:54:09 +0530

    Added Q38CountAndSay code

--- Strings/MediumQuestions/Q38CountAndSay.java
+++ Strings/MediumQuestions/Q38CountAndSay.java
@@ -13,7 +13,18 @@ replace "1" with "11". Thus the compressed string becomes "23321511".
 Given a positive integer n, return the nth element of the count-and-say sequence.
 
 Example 1:
+Input: n = 4
+Output: "1211"
 
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = RLE of "1" = "11"
+countAndSay(3) = RLE of "11" = "21"
+countAndSay(4) = RLE of "21" = "1211"
+
+Example 2:
+Input: n = 1
+Output: "1
 */
 
 public class Q38CountAndSay{

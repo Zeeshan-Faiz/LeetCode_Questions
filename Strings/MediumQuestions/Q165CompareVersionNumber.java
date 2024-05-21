96b5a2862a631cbcc5790c3bcb676d3356cd6e05
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-21 09:58:10 +0530

    Added Q165CompareVersionNumber code

--- Strings/MediumQuestions/Q165CompareVersionNumber.java
+++ Strings/MediumQuestions/Q165CompareVersionNumber.java
@@ -1,5 +1,21 @@
 package Strings.MediumQuestions;
 
+/*
+Given two version strings, version1 and version2, compare them. A version string consists of 
+revisions separated by dots '.'. The value of the revision is its integer conversion ignoring 
+leading zeros.
+To compare version strings, compare their revision values in left-to-right order. If one of the 
+version strings has fewer revisions, treat the missing revision values as 0.
+Return the following:
+
+    If version1 < version2, return -1.
+    If version1 > version2, return 1.
+    Otherwise, return 0.
+
+Example 1:
+
+*/
+
 public class Q165CompareVersionNumber {
     
 }

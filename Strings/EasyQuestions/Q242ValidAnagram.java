51960102e2ef4fa3e9970a99f80a4268d8d5b187
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-21 10:03:24 +0530

    Added Q242ValidAnagram code

--- Strings/EasyQuestions/Q242ValidAnagram.java
+++ Strings/EasyQuestions/Q242ValidAnagram.java
@@ -15,27 +15,25 @@ Output: false
 */
 
 public class Q242ValidAnagram {
-    
+
     public boolean isAnagram(String s, String t) {
-        int[] count = new int[26];
         
-        // Count the frequency of characters in string s
+        int[] count = new int[26];
+
+        // Count the frequency of characters in string s and string t
         for (char x : s.toCharArray()) {
             count[x - 'a']++;
         }
-        
-        // Decrement the frequency of characters in string t
         for (char x : t.toCharArray()) {
             count[x - 'a']--;
         }
-        
+
         // Check if any character has non-zero frequency
         for (int val : count) {
             if (val != 0) {
                 return false;
             }
         }
-        
         return true;
     }
 }

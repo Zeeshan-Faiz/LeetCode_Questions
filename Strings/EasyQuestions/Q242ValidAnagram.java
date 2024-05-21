1380ca97a54b0edf1b3eb1ec2a2ed40e44b77456
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-21 10:02:48 +0530

    Added Q242ValidAnagram code

--- Strings/EasyQuestions/Q242ValidAnagram.java
+++ Strings/EasyQuestions/Q242ValidAnagram.java
@@ -6,7 +6,12 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 typically using all the original letters exactly once.
 
 Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
 
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
 */
 
 public class Q242ValidAnagram {

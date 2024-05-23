34c2c459e27e8734010879db9a5bef4f217f4453
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-23 09:37:12 +0530

    Added Q144BinaryTreePreOrderTraversal code

--- Tree/EasyQuestions/Q144BinaryTreePreOrderTraversal.java
+++ Tree/EasyQuestions/Q144BinaryTreePreOrderTraversal.java
@@ -1,5 +1,8 @@
 package Tree.EasyQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
@@ -17,20 +20,34 @@ Output: [1]
 */
 
 public class Q144BinaryTreePreOrderTraversal {
-    
+
     List<Integer> result = new ArrayList<>();
+
     public List<Integer> preorderTraversal(TreeNode root) {
         preOrder(root);
         return result;
     }
 
-    private void preOrder(TreeNode node){
-        if(node == null){
+    private void preOrder(TreeNode node) {
+        if (node == null) {
             return;
         }
 
         result.add(node.val);
         preOrder(node.left);
         preOrder(node.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

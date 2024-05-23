756ab1010fb0b3ccbaaccd4ab4fdaa81603366e7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-23 09:39:36 +0530

    Added Q145BinaryTreePostOrderTraversal code

--- Tree/EasyQuestions/Q145BinaryTreePostOrderTraversal.java
+++ Tree/EasyQuestions/Q145BinaryTreePostOrderTraversal.java
@@ -1,5 +1,21 @@
 package Tree.EasyQuestions;
 
+/*
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+*/
+
 public class Q145BinaryTreePostOrderTraversal {
     
 }

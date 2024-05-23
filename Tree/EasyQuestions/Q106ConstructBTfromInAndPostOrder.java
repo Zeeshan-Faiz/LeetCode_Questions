301e3b19c68f37c766f7498bb669526cf0d6400c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-23 09:46:43 +0530

    Added Q106ConstructBTfromInAndPostOrder code

--- Tree/EasyQuestions/Q106ConstructBTfromInAndPostOrder.java
+++ Tree/EasyQuestions/Q106ConstructBTfromInAndPostOrder.java
@@ -14,26 +14,26 @@ Output: [-1]
 */
 
 public class Q106ConstructBTfromInAndPostOrder {
-    
-     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return buildTree(inorder, inorder.length-1, 0, postorder, postorder.length-1);
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return buildTree(inorder, inorder.length - 1, 0, postorder, postorder.length - 1);
     }
 
-private TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder,
-		int postStart) {
-	if (postStart < 0 || inStart < inEnd)
-		return null;
+    private TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart) {
+        
+        if (postStart < 0 || inStart < inEnd)
+            return null;
 
-	TreeNode root = new TreeNode(postorder[postStart]);
-	int rIndex = inStart;
-	for (int i = inStart; i >= inEnd; i--) {
-		if (inorder[i] == postorder[postStart]) {
-			rIndex = i;
-			break;
-		}
-	}
-	root.right = buildTree(inorder, inStart, rIndex + 1, postorder, postStart-1);
-	root.left = buildTree(inorder, rIndex - 1, inEnd, postorder, postStart - (inStart - rIndex) -1);
-	return root;
-  }
+        TreeNode root = new TreeNode(postorder[postStart]);
+        int rIndex = inStart;
+        for (int i = inStart; i >= inEnd; i--) {
+            if (inorder[i] == postorder[postStart]) {
+                rIndex = i;
+                break;
+            }
+        }
+        root.right = buildTree(inorder, inStart, rIndex + 1, postorder, postStart - 1);
+        root.left = buildTree(inorder, rIndex - 1, inEnd, postorder, postStart - (inStart - rIndex) - 1);
+        return root;
+    }
 }

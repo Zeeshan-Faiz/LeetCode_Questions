678acb7277c21b9f8a3d8fe30af6a6dd1e5ebda7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-24 10:02:06 +0530

    Added Q347TopKfreqElements code

--- Arrays/MeduimQuestions/Q347TopKfreqElements.java
+++ Arrays/MeduimQuestions/Q347TopKfreqElements.java
@@ -1,5 +1,13 @@
 package Arrays.MeduimQuestions;
 
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. You may return 
+the answer in any order.
+
+Example 1:
+
+*/
+
 public class Q347TopKfreqElements {
     
 }

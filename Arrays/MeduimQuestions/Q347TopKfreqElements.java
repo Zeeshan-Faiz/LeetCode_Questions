d4981840443c7ad789bd4c65c0e1568a382d0f0c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-24 10:05:06 +0530

    Added Q347TopKfreqElements code

--- Arrays/MeduimQuestions/Q347TopKfreqElements.java
+++ Arrays/MeduimQuestions/Q347TopKfreqElements.java
@@ -19,29 +19,30 @@ Output: [1]
 */
 
 public class Q347TopKfreqElements {
-    
+
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
         @SuppressWarnings("unchecked")
-        List<Integer> bucket[] = new ArrayList[nums.length + 1];                
-        
+        List<Integer> bucket[] = new ArrayList[nums.length + 1];
+
         for (int num : nums)
             count.merge(num, 1, Integer::sum);
-        
-        for (int key : count.keySet()){
+
+        for (int key : count.keySet()) {
             int freq = count.get(key);
             if (bucket[freq] == null)
                 bucket[freq] = new ArrayList<>();
             bucket[freq].add(key);
         }
-        
+
         int index = 0;
         int[] res = new int[k];
         for (int i = nums.length; i >= 0; i--)
             if (bucket[i] != null)
-                for (int val : bucket[i]){
+                for (int val : bucket[i]) 
+                {
                     res[index++] = val;
-                    if(index == k)
+                    if (index == k)
                         return res;
                 }
         return res;

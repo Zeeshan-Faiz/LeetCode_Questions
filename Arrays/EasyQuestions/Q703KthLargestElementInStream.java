8c2ab9c18b314d1f9b7025430d75914207a65419
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-25 17:31:15 +0530

    Added Q703KthLargestElementInStream code

--- Arrays/EasyQuestions/Q703KthLargestElementInStream.java
+++ Arrays/EasyQuestions/Q703KthLargestElementInStream.java
@@ -28,20 +28,22 @@ kthLargest.add(4);   // return 8
 */
 
 public class Q703KthLargestElementInStream {
-    
+
     final PriorityQueue<Integer> heap = new PriorityQueue<>();
     final int k;
-    
+
     public Q703KthLargestElementInStream(int k, int[] nums) {
         this.k = k;
-        for (int n : nums) add(n);
+        for (int n : nums)
+            add(n);
     }
 
     public int add(int val) {
-        if (heap.size() < k) heap.offer(val); //for adding the values of the array
+        if (heap.size() < k)
+            heap.offer(val); // for adding the values of the array
         else if (val > heap.peek()) {
-            heap.poll(); //remove the top element
-            heap.add(val); //add the new element
+            heap.poll(); // remove the top element
+            heap.add(val); // add the new element
         }
         return heap.peek();
     }

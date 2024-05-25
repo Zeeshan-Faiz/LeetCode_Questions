058120523041e7acd25c2d04415959d8d941b13c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-25 17:37:11 +0530

    Added Q322CoinChange code

--- DynamicProgramming/MediumQuestions/Q322CoinChange.java
+++ DynamicProgramming/MediumQuestions/Q322CoinChange.java
@@ -8,7 +8,17 @@ cannot be made up by any combination of the coins, return -1. You may assume tha
 infinite number of each kind of coin.
 
 Example 1:
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
 
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+
+Example 3:
+Input: coins = [1], amount = 0
+Output: 0
 */
 
 public class Q322CoinChange {

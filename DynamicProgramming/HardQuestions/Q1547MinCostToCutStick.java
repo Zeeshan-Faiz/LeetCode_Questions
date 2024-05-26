81de8682bbc281ccd65ec567c96581ca117607b9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-26 12:56:13 +0530

    Added Q1547MinCostToCutStick code

--- DynamicProgramming/HardQuestions/Q1547MinCostToCutStick.java
+++ DynamicProgramming/HardQuestions/Q1547MinCostToCutStick.java
@@ -1,5 +1,19 @@
 package DynamicProgramming.HardQuestions;
 
+/*
+Given a wooden stick of length n units. The stick is labelled from 0 to n. For example, 
+a stick of length 6 is labelled as follows:
+Given an integer array cuts where cuts[i] denotes a position you should perform a cut at. The cost 
+of one cut is the length of the stick to be cut, the total cost is the sum of costs of all cuts. 
+When you cut a stick, it will be split into two smaller sticks (i.e. the sum of their lengths is 
+the length of the stick before the cut). Please refer to the first example for a better explanation.
+
+Return the minimum total cost of the cuts.
+
+Example 1:
+
+*/
+
 public class Q1547MinCostToCutStick {
     
 }

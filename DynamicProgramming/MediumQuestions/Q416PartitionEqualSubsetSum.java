a33c44a5032ef77da63b4a5f3cfb69ee106f5426
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-26 12:36:14 +0530

    Added Q416PartitionEqualSubsetSum code

--- DynamicProgramming/MediumQuestions/Q416PartitionEqualSubsetSum.java
+++ DynamicProgramming/MediumQuestions/Q416PartitionEqualSubsetSum.java
@@ -1,5 +1,13 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+Given an integer array nums, return true if you can partition the array into two subsets such 
+that the sum of the elements in both subsets is equal or false otherwise.
+
+Example 1:
+
+*/
+
 public class Q416PartitionEqualSubsetSum {
     
 }

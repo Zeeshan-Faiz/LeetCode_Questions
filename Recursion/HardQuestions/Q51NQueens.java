63c5de310440ebacc8539f1d4eb3d2380890502e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-27 06:29:11 +0530

    Added Q51NQueens code

--- Recursion/HardQuestions/Q51NQueens.java
+++ Recursion/HardQuestions/Q51NQueens.java
@@ -1,5 +1,8 @@
 package Recursion.HardQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two 
 queens attack each other.
@@ -20,4 +23,18 @@ Output: [["Q"]]
 
 public class Q51NQueens {
     
+    public List<List<String>> solveNQueens(int n) {
+         List<List<String>> res = new ArrayList<>();
+		char[][] board = new char[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				board[i][j] = '.';
+			}
+		}
+		int[] leftRow = new int[n];
+		int[] upperDiagonal = new int[2 * n - 1];
+		int[] lowerDiagonal = new int[2 * n - 1];
+		nQueens(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
+		return res;
+    }
 }

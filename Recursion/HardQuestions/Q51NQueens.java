1d46514c7ca4313938bdce620a81648bf0a71a47
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-27 06:28:47 +0530

    Added Q51NQueens code

--- Recursion/HardQuestions/Q51NQueens.java
+++ Recursion/HardQuestions/Q51NQueens.java
@@ -9,7 +9,13 @@ Each solution contains a distinct board configuration of the n-queens' placement
 and '.' both indicate a queen and an empty space, respectively.
 
 Example 1:
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
 
+Example 2:
+Input: n = 1
+Output: [["Q"]]
 */
 
 public class Q51NQueens {

746644e3fe913ae7cedf042689b18e9c0ab45fd5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-29 06:31:54 +0530

    Added Q239SlidingWindowMaximum code

--- StackandQueue/HardQuestions/Q239SlidingWindowMaximum.java
+++ StackandQueue/HardQuestions/Q239SlidingWindowMaximum.java
@@ -13,26 +13,28 @@ Output: [3,3,5,5,6,7]
 */
 
 public class Q239SlidingWindowMaximum {
-    
+
     public int[] maxSlidingWindow(int[] nums, int k) {
+        
         int n = nums.length;
         int[] res = new int[n - k + 1];
         ArrayDeque<Integer> deque = new ArrayDeque<>();
-        for (int i = k - 1; i >= 0; i--) {
-            if (deque.isEmpty()) {
+        for (int i = k - 1; i >= 0; i--) 
+        {
+            if (deque.isEmpty())
                 deque.addFirst(i);
-            } else {
-                if (nums[i] > nums[deque.getLast()]) {
+            else {
+                if (nums[i] > nums[deque.getLast()])
                     deque.addLast(i);
-                }
             }
         }
+        
         res[0] = nums[deque.getLast()];
-
-        for (int i = k; i < n; i++) {
-            if (deque.getLast() == i - k) {
+        for (int i = k; i < n; i++) 
+        {
+            if (deque.getLast() == i - k)
                 deque.removeLast();
-            }
+                
             int candidate = nums[i];
             while (!deque.isEmpty() && nums[deque.getFirst()] < candidate) {
                 deque.removeFirst();

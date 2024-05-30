e7da33bddcbba923acbe58596ca7315bf599f3d3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-30 10:23:26 +0530

    Added Q901OnlineStockSpan code

--- StackandQueue/MediumQuestions/Q901OnlineStockSpan.java
+++ StackandQueue/MediumQuestions/Q901OnlineStockSpan.java
@@ -23,4 +23,24 @@ Output
 
 public class Q901OnlineStockSpan {
     
+    int N = 10000;
+    int cur, tt;
+    int[] stk, stock;
+
+    public Q901OnlineStockSpan() {
+        cur = 0;
+        tt = -1;
+        stk = new int[N];
+        stock = new int[N];
+    }
+    
+    public int next(int price) {
+        while(tt >= 0 && price >= stock[stk[tt]]) {
+            tt--;
+        }
+        int res = tt < 0 ? cur + 1 : cur - stk[tt];
+        stk[++tt] = cur;
+        stock[cur++] = price;
+        return res;
+    }
 }

1910340f330e1284efa1a4d9c657f6c9b5129228
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-05-31 11:40:08 +0530

    Added Q152MaxProductSubArray code

--- DynamicProgramming/MediumQuestions/Q152MaxProductSubArray.java
+++ DynamicProgramming/MediumQuestions/Q152MaxProductSubArray.java
@@ -1,5 +1,13 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Example 1:
+
+*/
+
 public class Q152MaxProductSubArray {
     
 }

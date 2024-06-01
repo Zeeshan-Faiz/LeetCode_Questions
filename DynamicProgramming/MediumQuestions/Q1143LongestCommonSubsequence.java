3c13a737c8ad819025013304a9ad69ed5fea3aa4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-01 10:59:16 +0530

    Added Q1143LongestCommonSubsequence code

--- DynamicProgramming/MediumQuestions/Q1143LongestCommonSubsequence.java
+++ DynamicProgramming/MediumQuestions/Q1143LongestCommonSubsequence.java
@@ -9,7 +9,19 @@ A subsequence of a string is a new string generated from the original string wit
 A common subsequence of two strings is a subsequence that is common to both strings.
 
 Example 1:
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
 
+Example 2:
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+
+Example 3:
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
 */
 
 public class Q1143LongestCommonSubsequence {

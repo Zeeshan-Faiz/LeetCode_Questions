587544c32f75c966b87417e19992435a84f7bada
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-02 14:45:28 +0530

    Added Q875KokoEatingBananas code

--- BinarySearch/MediumQuestions/Q875KokoEatingBananas.java
+++ BinarySearch/MediumQuestions/Q875KokoEatingBananas.java
@@ -10,7 +10,16 @@ Koko likes to eat slowly but still wants to finish eating all the bananas before
 Return the minimum integer k such that she can eat all the bananas within h hours.
 
 Example 1:
+Input: piles = [3,6,7,11], h = 8
+Output: 4
 
+Example 2:
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+
+Example 3:
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
 */
 
 public class Q875KokoEatingBananas {

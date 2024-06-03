159eab56232d496475b1a3a013e09c90fb658445
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-03 10:32:33 +0530

    Added Q516LongestPalindromicSubsequence code

--- DynamicProgramming/MediumQuestions/Q516LongestPalindromicSubsequence.java
+++ DynamicProgramming/MediumQuestions/Q516LongestPalindromicSubsequence.java
@@ -17,30 +17,30 @@ Explanation: One possible longest palindromic subsequence is "bb".
 */
 
 public class Q516LongestPalindromicSubsequence {
-    
+
     int[][] dp;
     int n;
     public int longestPalindromeSubseq(String s) {
-            n = s.length();
-            dp = new int[n][n];
-            return helper(0, n-1, s);
-        }
+        n = s.length();
+        dp = new int[n][n];
+        return helper(0, n - 1, s);
+    }
 
-    int helper(int i, int j, String s){
-        if(i>j) {
+    int helper(int i, int j, String s) {
+        
+        if (i > j)
             return 0;
-        }
-        if(i==j){
+    
+        if (i == j)
             return 1;
-        }
-        if(dp[i][j]!=0){
+    
+        if (dp[i][j] != 0)
             return dp[i][j];
-        }
-        if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = 2 + helper(i+1, j-1, s);
-        }
-        else{
-            return dp[i][j] = Math.max(helper(i+1, j, s), helper(i, j-1, s));
-        }
+
+        if (s.charAt(i) == s.charAt(j))
+            return dp[i][j] = 2 + helper(i + 1, j - 1, s);
+        
+        else 
+            return dp[i][j] = Math.max(helper(i + 1, j, s), helper(i, j - 1, s));
     }
 }

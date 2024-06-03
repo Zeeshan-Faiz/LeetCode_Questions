1d550e43b97da7760d53c27c0d556704561f7de3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-03 10:31:14 +0530

    Added Q516LongestPalindromicSubsequence code

--- DynamicProgramming/MediumQuestions/Q516LongestPalindromicSubsequence.java
+++ DynamicProgramming/MediumQuestions/Q516LongestPalindromicSubsequence.java
@@ -1,5 +1,21 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+Given a string s, find the longest palindromic subsequence's length in s.
+A subsequence is a sequence that can be derived from another sequence by deleting some or no 
+elements without changing the order of the remaining elements.
+
+Example 1:
+Input: s = "bbbab"
+Output: 4
+Explanation: One possible longest palindromic subsequence is "bbbb".
+
+Example 2:
+Input: s = "cbbd"
+Output: 2
+Explanation: One possible longest palindromic subsequence is "bb".
+*/
+
 public class Q516LongestPalindromicSubsequence {
     
 }

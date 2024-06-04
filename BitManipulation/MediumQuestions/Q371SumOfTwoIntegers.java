de526833585ba79388dd19d638221b20d4e3a150
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-04 10:22:00 +0530

    Added Q371SumOfTwoIntegers code

--- BitManipulation/MediumQuestions/Q371SumOfTwoIntegers.java
+++ BitManipulation/MediumQuestions/Q371SumOfTwoIntegers.java
@@ -14,4 +14,16 @@ Output: 5
 
 public class Q371SumOfTwoIntegers{
     
+    public int getSum(int a, int b) {
+        if (a == 0) return b;
+	if (b == 0) return a;
+
+	while (b != 0) {
+		int carry = a & b;
+		a = a ^ b;
+		b = carry << 1;
+	}
+	
+	return a;
+    }
 }

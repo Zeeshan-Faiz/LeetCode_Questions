d29996053e79c3ae42f8e208384209a1c6f53c09
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-04 10:21:38 +0530

    Added Q371SumOfTwoIntegers code

--- BitManipulation/MediumQuestions/Q371SumOfTwoIntegers.java
+++ BitManipulation/MediumQuestions/Q371SumOfTwoIntegers.java
@@ -1,5 +1,12 @@
 package BitManipulation.MediumQuestions;
 
+/*
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+Example 1:
+
+*/
+
 public class Q371SumOfTwoIntegers{
     
 }

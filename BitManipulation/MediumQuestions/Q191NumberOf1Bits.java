13d8c4f1589937d2cde0f0343ee16b7fbddcc07e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-04 10:25:10 +0530

    Added Q191NumberOf1Bits code

--- BitManipulation/MediumQuestions/Q191NumberOf1Bits.java
+++ BitManipulation/MediumQuestions/Q191NumberOf1Bits.java
@@ -1,5 +1,13 @@
 package BitManipulation.MediumQuestions;
 
+/*
+Write a function that takes the binary representation of a positive integer and returns the number of
+set bits it has (also known as the Hamming weight).
+
+Example 1:
+
+*/
+
 public class Q191NumberOf1Bits {
     
 }

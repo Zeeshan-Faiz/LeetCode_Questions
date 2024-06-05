df9f7a3fc30cc72eee34a1cd11afe711b1dd9316
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-05 10:13:41 +0530

    Added Q338CountingBits code

--- BitManipulation/EasyQuestions/Q338CountingBits.java
+++ BitManipulation/EasyQuestions/Q338CountingBits.java
@@ -27,4 +27,11 @@ Explanation:
 
 public class Q338CountingBits {
     
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
 }

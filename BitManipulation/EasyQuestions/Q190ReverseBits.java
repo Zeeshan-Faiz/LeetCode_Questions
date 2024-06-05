a7ecd60d9d1fcd260e4e934e3e0f3d63b1112597
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-05 10:23:11 +0530

    Added Q190ReverseBits code

--- BitManipulation/EasyQuestions/Q190ReverseBits.java
+++ BitManipulation/EasyQuestions/Q190ReverseBits.java
@@ -25,4 +25,16 @@ Explanation: The input binary string 11111111111111111111111111111101 represents
 
 public class Q190ReverseBits {
     
+    public int reverseBits(int n) {
+        int result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            result = result << 1;
+            if ((n & 1) == 1) {
+                result += 1;
+            }
+            n = n >> 1;
+        }
+        return result;
+    }
 }

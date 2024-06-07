fdffa800e9397d765ce3650e8fe1400236f1f706
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-07 10:15:33 +0530

    Added Q1283FindSmalestDivGreaterThanThreshold code

--- BinarySearch/MediumQuestions/Q1283FindSmallestDivGreaterThanThreshold.java
+++ BinarySearch/MediumQuestions/Q1283FindSmallestDivGreaterThanThreshold.java
@@ -9,7 +9,14 @@ element. (For example: 7/3 = 3 and 10/2 = 5).
 The test cases are generated so that there will be an answer.
 
 Example 1:
+Input: nums = [1,2,5,9], threshold = 6
+Output: 5
+Explanation: We can get a sum to 17 (1+2+5+9) if the divisor is 1. 
+If the divisor is 4 we can get a sum of 7 (1+1+2+3) and if the divisor is 5 the sum will be 5 (1+1+1+2). 
 
+Example 2:
+Input: nums = [44,22,33,11,1], threshold = 5
+Output: 44
 */
 
 public class Q1283FindSmallestDivGreaterThanThreshold {

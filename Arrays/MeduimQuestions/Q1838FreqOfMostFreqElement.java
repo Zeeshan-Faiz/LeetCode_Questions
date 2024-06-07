779bd092d3465044fdf7952979e473914eafaffb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-07 10:08:06 +0530

    Added Q1838FreqOfMostFreqElement code

--- Arrays/MeduimQuestions/Q1838FreqOfMostFreqElement.java
+++ Arrays/MeduimQuestions/Q1838FreqOfMostFreqElement.java
@@ -24,18 +24,20 @@ Explanation: There are multiple optimal solutions:
 */
 
 public class Q1838FreqOfMostFreqElement {
-    
+
     public int maxFrequency(int[] nums, int k) {
+        
         Arrays.sort(nums);
         int left = 0, right = 0;
         int n = nums.length;
-        long sum = 0;  // Using long to avoid overflow
+        long sum = 0; // Using long to avoid overflow
 
-        while (right < n) {
-            sum += nums[right];
+        while (right < n) 
+        {
+            sum = sum + nums[right];
 
             // Check if the current window can be made into an array with all elements as nums[right]
-            long requiredSum = (long)nums[right] * (right - left + 1);
+            long requiredSum = (long) nums[right] * (right - left + 1);
             long diff = requiredSum - sum;
 
             if (diff > k) {
@@ -44,7 +46,6 @@ public class Q1838FreqOfMostFreqElement {
             }
             right++;
         }
-
         return right - left;
     }
 }

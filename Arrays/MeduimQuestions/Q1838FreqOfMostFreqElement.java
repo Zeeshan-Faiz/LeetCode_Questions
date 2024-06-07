73b7b9a57c607242dda614f64496684c3bfdcc7a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-07 10:07:44 +0530

    Added Q1838FreqOfMostFreqElement code

--- Arrays/MeduimQuestions/Q1838FreqOfMostFreqElement.java
+++ Arrays/MeduimQuestions/Q1838FreqOfMostFreqElement.java
@@ -1,5 +1,7 @@
 package Arrays.MeduimQuestions;
 
+import java.util.Arrays;
+
 /*
 The frequency of an element is the number of times it occurs in an array.
 You are given an integer array nums and an integer k. In one operation, you can choose an index 
@@ -23,4 +25,26 @@ Explanation: There are multiple optimal solutions:
 
 public class Q1838FreqOfMostFreqElement {
     
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0, right = 0;
+        int n = nums.length;
+        long sum = 0;  // Using long to avoid overflow
+
+        while (right < n) {
+            sum += nums[right];
+
+            // Check if the current window can be made into an array with all elements as nums[right]
+            long requiredSum = (long)nums[right] * (right - left + 1);
+            long diff = requiredSum - sum;
+
+            if (diff > k) {
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+
+        return right - left;
+    }
 }

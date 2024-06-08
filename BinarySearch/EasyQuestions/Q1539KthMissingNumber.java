a7cd423a8d7a60d2fac573095ad3cf6d3d05796e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-08 12:37:15 +0530

    Added Q1539KthMissingNumber code

--- BinarySearch/EasyQuestions/Q1539KthMissingNumber.java
+++ BinarySearch/EasyQuestions/Q1539KthMissingNumber.java
@@ -1,5 +1,13 @@
 package BinarySearch.EasyQuestions;
 
+/*
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Return the kth positive integer that is missing from this array.
+
+Example 1:
+
+*/
+
 public class Q1539KthMissingNumber {
     
 }

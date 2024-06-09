d04f1985cc4585354f42b54665cbc07a6402cbdf
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-09 10:51:50 +0530

    Added Q796RotateString code

--- Strings/EasyQuestions/Q796RotateString.java
+++ Strings/EasyQuestions/Q796RotateString.java
@@ -1,5 +1,15 @@
 package Strings.EasyQuestions;
 
+/*
+Given two strings s and goal, return true if and only if s can become goal after some number of 
+shifts on s.
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+    For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+Example 1:
+
+*/
+
 public class Q796RotateString {
     
 }

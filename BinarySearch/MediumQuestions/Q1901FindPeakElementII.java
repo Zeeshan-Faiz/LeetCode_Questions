e352e63d6c6e2bedbb99634ef0db72e20bc17217
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-09 10:46:06 +0530

    Added Q1901FindPeakElementII code

--- BinarySearch/MediumQuestions/Q1901FindPeakElementII.java
+++ BinarySearch/MediumQuestions/Q1901FindPeakElementII.java
@@ -8,7 +8,14 @@ You may assume that the entire matrix is surrounded by an outer perimeter with t
 each cell. You must write an algorithm that runs in O(m log(n)) or O(n log(m)) time.
 
 Example 1:
+Input: mat = [[1,4],[3,2]]
+Output: [0,1]
+Explanation: Both 3 and 4 are peak elements so [1,0] and [0,1] are both acceptable answers.
 
+Example 2:
+Input: mat = [[10,20,15],[21,30,14],[7,16,32]]
+Output: [1,1]
+Explanation: Both 30 and 32 are peak elements so [1,1] and [2,2] are both acceptable answers.
 */
 
 public class Q1901FindPeakElementII {

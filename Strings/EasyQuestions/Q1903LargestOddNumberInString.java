df74a9150fa1534f9ee97b20ca5dbd4c631d1e9c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-10 10:50:03 +0530

    Added Q1903LargestOddNumberInString code

--- Strings/EasyQuestions/Q1903LargestOddNumberInString.java
+++ Strings/EasyQuestions/Q1903LargestOddNumberInString.java
@@ -1,5 +1,14 @@
 package Strings.EasyQuestions;
 
+/*
+You are given a string num, representing a large integer. Return the largest-valued odd integer 
+(as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+*/
+
 public class Q1903LargestOddNumberInString {
     
 }

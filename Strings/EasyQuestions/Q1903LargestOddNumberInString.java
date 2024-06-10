69f871d2a4f27c6dc9a637ed439ed214bf7b53b6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-10 10:50:16 +0530

    Added Q1903LargestOddNumberInString code

--- Strings/EasyQuestions/Q1903LargestOddNumberInString.java
+++ Strings/EasyQuestions/Q1903LargestOddNumberInString.java
@@ -6,7 +6,19 @@ You are given a string num, representing a large integer. Return the largest-val
 A substring is a contiguous sequence of characters within a string.
 
 Example 1:
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
 
+Example 2:
+Input: num = "4206"
+Output: ""
+Explanation: There are no odd numbers in "4206".
+
+Example 3:
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
 */
 
 public class Q1903LargestOddNumberInString {

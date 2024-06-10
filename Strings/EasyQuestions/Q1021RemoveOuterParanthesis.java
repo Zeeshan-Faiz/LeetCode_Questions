124fb9668f8be890b1cca638bf608cb7f6f19c6d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-10 10:46:21 +0530

    Added Q1021RemoveOuterParanthesis code

--- Strings/EasyQuestions/Q1021RemoveOuterParanthesis.java
+++ Strings/EasyQuestions/Q1021RemoveOuterParanthesis.java
@@ -36,22 +36,22 @@ After removing outer parentheses of each part, this is "" + "" = "".
 public class Q1021RemoveOuterParanthesis {
     
     public String removeOuterParentheses(String s) {
-        int len = s.length();
-       if (len <= 2) return "";
-       char[] c = s.toCharArray();
-       StringBuilder newString = new StringBuilder();
-       int open = 1;
-       int openLeft = 0;
-       for (int i = 1; i < len; i++) {
-           if (c[i] == '(') {
-               open++;
-               if (open > 1) newString.append('(');
-           }
-           else {
-               if (open > 1) newString.append(')');
-               open--;
-           }
-       }
-       return newString.toString();
+        int len=s.length();
+        if(len<=2)
+        return "";
+        char[] c=s.toCharArray();
+        StringBuilder str=new StringBuilder();
+        int count=0;
+        for(int i=0;i<len;i++){
+            if(c[i]=='('){
+                count++;
+                if(count>1)str.append('(');
+            }
+            else{
+                if(count>1)str.append(')');
+                count--;
+            }
+        }
+        return str.toString();
    }
 }

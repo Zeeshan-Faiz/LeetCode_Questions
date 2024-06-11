6d10a38946eb139e62d06cb435e512ac9b72d330
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-11 10:26:20 +0530

    Added Q1614MaxNestingDepth code

--- Strings/EasyQuestions/Q1614MaxNestingDepth.java
+++ Strings/EasyQuestions/Q1614MaxNestingDepth.java
@@ -1,5 +1,13 @@
 package Strings.EasyQuestions;
 
+/*
+Given a valid parentheses string s, return the nesting depth of s. The nesting depth is the 
+maximum number of nested parentheses.
+
+Example 1:
+
+*/
+
 public class Q1614MaxNestingDepth {
     
 }

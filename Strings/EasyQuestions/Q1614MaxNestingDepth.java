34933d0e83ee48e4a93bddd7863f990eabaa5a87
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-11 10:26:59 +0530

    Added Q1614MaxNestingDepth code

--- Strings/EasyQuestions/Q1614MaxNestingDepth.java
+++ Strings/EasyQuestions/Q1614MaxNestingDepth.java
@@ -23,4 +23,17 @@ Output: 3
 
 public class Q1614MaxNestingDepth {
     
+    public int maxDepth(String s) {
+        int ans = 0;
+        int count = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                count++;
+                ans = Math.max(ans,count);
+            }else if (ch == ')'){
+                count--;
+            }
+        }
+        return ans;
+    }
 }

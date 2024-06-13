aa2651dd5e321d869482177375e5152043e159d5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-13 10:25:22 +0530

    Added Q231PowerOf2 code

--- BitManipulation/EasyQuestions/Q231PowerOf2.java
+++ BitManipulation/EasyQuestions/Q231PowerOf2.java
@@ -20,5 +20,12 @@ Output: false
 */
 
 public class Q231PowerOf2 {
-    
+
+    public boolean isPowerOfTwo(int n) {
+
+        if (n == 0 || n < 0)
+            return false;
+        else
+            return (n & (n - 1)) == 0;
+    }
 }

c4555f7808a2db30a84288f52c71119d2e6d130c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-13 10:22:06 +0530

    Added Q282ExpressionsAndOperators code

--- Recursion/HardQuestions/Q282ExpressionsAndOperators.java
+++ Recursion/HardQuestions/Q282ExpressionsAndOperators.java
@@ -1,5 +1,15 @@
 package Recursion.HardQuestions;
 
+/*
+Given a string num that contains only digits and an integer target, return all possibilities to 
+insert the binary operators '+', '-', and/or '*' between the digits of num so that the resultant 
+expression evaluates to the target value.
+Note that operands in the returned expressions should not contain leading zeros.
+
+Example 1:
+
+*/
+
 public class Q282ExpressionsAndOperators {
     
 }

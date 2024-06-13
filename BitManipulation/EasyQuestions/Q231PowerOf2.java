fe4306b3d4ed8219f54dcb1f946d9e72245bb1c3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-13 10:25:07 +0530

    Added Q231PowerOf2 code

--- BitManipulation/EasyQuestions/Q231PowerOf2.java
+++ BitManipulation/EasyQuestions/Q231PowerOf2.java
@@ -5,7 +5,18 @@ Given an integer n, return true if it is a power of two. Otherwise, return false
 a power of two, if there exists an integer x such that n == 2x
 
 Example 1:
+Input: n = 1
+Output: true
+Explanation: 20 = 1
 
+Example 2:
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+
+Example 3:
+Input: n = 3
+Output: false
 */
 
 public class Q231PowerOf2 {

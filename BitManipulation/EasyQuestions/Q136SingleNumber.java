c27fa5ea4c2c6540cf7cc23be21ca60016dda04e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-14 10:19:39 +0530

    Added Q136SingleNumber code

--- BitManipulation/EasyQuestions/Q136SingleNumber.java
+++ BitManipulation/EasyQuestions/Q136SingleNumber.java
@@ -20,4 +20,11 @@ Output: 1
 
 public class Q136SingleNumber {
     
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0;i<nums.length;i++){
+            result^=nums[i];
+        }
+        return result;
+    }
 }

cddac8309f1777c96b74aed07ed113ddddbbea37
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-14 10:23:34 +0530

    Added Q204CountPrimes code

--- BitManipulation/MediumQuestions/Q204CountPrimes.java
+++ BitManipulation/MediumQuestions/Q204CountPrimes.java
@@ -1,5 +1,12 @@
 package BitManipulation.MediumQuestions;
 
+/*
+Given an integer n, return the number of prime numbers that are strictly less than n.
+
+Example 1:
+
+*/
+
 public class Q204CountPrimes {
     
 }

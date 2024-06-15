3bbbff74934bd6f7ba28e39106c629953fec540f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-15 09:39:43 +0530

    Added Q146LRUCache code

--- LinkedList/MeduimQuestions/Q146LRUCache.java
+++ LinkedList/MeduimQuestions/Q146LRUCache.java
@@ -33,107 +33,106 @@ lRUCache.get(4);    // return 4
 */
 
 public class Q146LRUCache {
-    
-    class Node{
+
+    class Node {
         int key;
         int value;
 
         Node prev;
         Node next;
 
-        Node(int key, int value){
-            this.key= key;
-            this.value= value;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
         }
     }
 
     public Node[] map;
     public int count, capacity;
     public Node head, tail;
-    
-    public LRUCache(int capacity) {
-        
-        this.capacity= capacity;
-        count= 0;
-        
-        map= new Node[10_000+1];
-        
-        head= new Node(0,0);
-        tail= new Node(0,0);
-        
-        head.next= tail;
-        tail.prev= head;
-        
-        head.prev= null;
-        tail.next= null;
+
+    public Q146LRUCache(int capacity) {
+
+        this.capacity = capacity;
+        count = 0;
+
+        map = new Node[10_000 + 1];
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+
+        head.prev = null;
+        tail.next = null;
     }
-    
-    public void deleteNode(Node node){
-        node.prev.next= node.next;
-        node.next.prev= node.prev;       
-        
+
+    public void deleteNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+
         return;
     }
-    
-    public void addToHead(Node node){
-        node.next= head.next;
-        node.next.prev= node;
-        node.prev= head;
-        
-        head.next= node;      
-        
+
+    public void addToHead(Node node) {
+        node.next = head.next;
+        node.next.prev = node;
+        node.prev = head;
+
+        head.next = node;
+
         return;
     }
-    
+
     public int get(int key) {
-        
-        if( map[key] != null ){
-            
-            Node node= map[key];
-            
-            int nodeVal= node.value;
-            
+
+        if (map[key] != null) {
+
+            Node node = map[key];
+
+            int nodeVal = node.value;
+
             deleteNode(node);
-            
+
             addToHead(node);
-            
+
             return nodeVal;
-        }
+        } 
         else
             return -1;
     }
-    
+
     public void put(int key, int value) {
-        
-        if(map[key] != null){
-            
-            Node node= map[key];
-            
-            node.value= value;
-            
+
+        if (map[key] != null) {
+
+            Node node = map[key];
+
+            node.value = value;
+
             deleteNode(node);
-            
+
             addToHead(node);
-            
+
         } else {
-            
-            Node node= new Node(key,value);
-            
-            map[key]= node;
-            
-            if(count < capacity){
+
+            Node node = new Node(key, value);
+
+            map[key] = node;
+
+            if (count < capacity) {
                 count++;
                 addToHead(node);
-            } 
-            else {
-                
-                map[tail.prev.key]= null;
+            } else {
+
+                map[tail.prev.key] = null;
                 deleteNode(tail.prev);
-                
+
                 addToHead(node);
             }
         }
-        
+
         return;
     }
 }

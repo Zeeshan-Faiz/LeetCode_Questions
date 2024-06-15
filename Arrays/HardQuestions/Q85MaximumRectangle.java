3c04ad161ec4b345894965588763dbbc51268ab9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-15 09:33:16 +0530

    Added Q85MaximumRectangle code

--- Arrays/HardQuestions/Q85MaximumRectangle.java
+++ Arrays/HardQuestions/Q85MaximumRectangle.java
@@ -1,5 +1,13 @@
 package Arrays.HardQuestions;
 
+/*
+Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle 
+containing only 1's and return its area.
+
+Example 1:
+
+*/
+
 public class Q85MaximumRectangle {
     
 }

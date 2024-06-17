32ecf349317aabcd23a557401f3263c0a3002ae4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-17 11:17:19 +0530

    Added Q295FindMedianFromDataStream code

--- StackandQueue/HardQuestions/Q295FindMedianFromDataStream.java
+++ StackandQueue/HardQuestions/Q295FindMedianFromDataStream.java
@@ -28,57 +28,63 @@ medianFinder.addNum(3);    // arr[1, 2, 3]
 medianFinder.findMedian(); // return 2.0
 */
 
-public class Q295FindMedianFromDataStream{
-    
+public class Q295FindMedianFromDataStream {
+
     class MedianFinder {
 
         int[] hashVals;
         int median;
         int medianCount2x;
-    
+
         public MedianFinder() {
             hashVals = new int[200001];
             median = Integer.MIN_VALUE;
             medianCount2x = 1;
         }
-        
+
         public void addNum(int num) {
-            hashVals[num+100000]++;
+            
+            hashVals[num + 100000]++;
             if (median != Integer.MIN_VALUE) {
                 if (num > median) {
-                    if (medianCount2x < 2*hashVals[median+100000]) {
+                    if (medianCount2x < 2 * hashVals[median + 100000]) {
                         medianCount2x++;
                     } else {
                         medianCount2x = 1;
-                        while (hashVals[++median+100000] == 0) {}
+                        while (hashVals[++median + 100000] == 0) {
+                        }
                     }
                 } else if (num < median) {
                     if (medianCount2x > 1) {
                         medianCount2x--;
                     } else {
-                        while (hashVals[--median + 100000] ==0) {}
-                        medianCount2x = 2*hashVals[median+100000];
+                        while (hashVals[--median + 100000] == 0) {
+                        }
+                        medianCount2x = 2 * hashVals[median + 100000];
                     }
-                } else {
+                } 
+                else
                     medianCount2x++;
-                }
-            } else {
+            } 
+            else
                 median = num;
-            }
         }
-        
+
         public double findMedian() {
-            if (medianCount2x % 2 == 0) {
-                if (2*hashVals[median+100000] > medianCount2x) {
+
+            if (medianCount2x % 2 == 0) 
+            {
+                if (2 * hashVals[median + 100000] > medianCount2x) {
                     return median;
                 } else {
                     int median2 = median;
-                    while (hashVals[++median2 + 100000] == 0) {}
-                    return (median + median2)/2.0;
+                    while (hashVals[++median2 + 100000] == 0) {
+                    }
+                    return (median + median2) / 2.0;
                 }
-            } else {
+            } 
+            else
                 return median;
-            }
         }
-    }    
+    }
 }

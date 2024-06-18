7ea46549fa841485cd18832b18c268117a7f0580
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-18 14:23:50 +0530

    Added Q863AllNodesDistanceK code

--- Tree/EasyQuestions/Q863AllNodesDistanceK.java
+++ Tree/EasyQuestions/Q863AllNodesDistanceK.java
@@ -1,5 +1,14 @@
 package Tree.EasyQuestions;
 
+/*
+Given the root of a binary tree, the value of a target node target, and an integer k, return 
+an array of the values of all nodes that have a distance k from the target node.
+You can return the answer in any order.
+
+Example 1:
+
+*/
+
 public class Q863AllNodesDistanceK {
     
 }

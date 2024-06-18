8dc8d0e6ceccf0920bc8bbaa245099d766dbc071
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-18 14:17:37 +0530

    Added Q860LemonadeChange code

--- Greedy/EasyQuestions/Q860LemonadeChange.java
+++ Greedy/EasyQuestions/Q860LemonadeChange.java
@@ -31,4 +31,30 @@ Since not every customer received the correct change, the answer is false.
 
 public class Q860LemonadeChange {
     
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        int length=bills.length;
+        for(int i=0;i<length;i++){
+            if(bills[i]==5) five++;
+            else if(bills[i]==10){
+                if(five>0){
+                    five--;
+                    ten++;
+                }
+                else return false;
+            }
+            else{
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
 }

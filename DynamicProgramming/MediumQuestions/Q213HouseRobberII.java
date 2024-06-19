e8e2bf06f9322fd77d1a37b6616b2b602ddb2ab2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-19 14:02:42 +0530

    Added Q213HouseRobberII code

--- DynamicProgramming/MediumQuestions/Q213HouseRobberII.java
+++ DynamicProgramming/MediumQuestions/Q213HouseRobberII.java
@@ -26,34 +26,33 @@ Output: 3
 */
 
 public class Q213HouseRobberII {
-    
-    public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 1){
-            return nums[0];
-        }else if(n == 2){
-            return Math.max(nums[0], nums[1]);
-        }
 
-        int [] dp1 = new int [n+1]; // not allowed to take final 
+    public int rob(int[] nums) {
+        
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        else if (n == 2)
+            return Math.max(nums[0], nums[1]);
+
+        int[] dp1 = new int[n + 1]; // not allowed to take final
         dp1[0] = nums[0];
         dp1[1] = Math.max(nums[0], nums[1]);
 
-
-        int [] dp2 = new int [n+1]; //allowed to take final
+        int[] dp2 = new int[n + 1]; // allowed to take final
         dp2[0] = 0;
         dp2[1] = nums[1];
 
         // dp1
-        for(int i = 2; i < n-1; i++){
-            dp1[i] = Math.max(dp1[i-1], dp1[i-2]+ nums[i]);
+        for (int i = 2; i < n - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + nums[i]);
         }
 
         // dp2
-        for(int i = 2; i < n; i++){
-            dp2[i] = Math.max(dp2[i-1], dp2[i-2]+ nums[i]);
+        for (int i = 2; i < n; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + nums[i]);
         }
 
-        return Math.max(dp1[n-2], dp2[n-1]);
+        return Math.max(dp1[n - 2], dp2[n - 1]);
     }
 }

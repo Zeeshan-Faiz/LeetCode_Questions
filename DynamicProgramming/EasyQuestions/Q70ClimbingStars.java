f62a00b8a23fe8c47c1862fe2b8bce129c721a6c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-19 13:54:57 +0530

    Added Q70ClimbingStars code

--- DynamicProgramming/EasyQuestions/Q70ClimbingStars.java
+++ DynamicProgramming/EasyQuestions/Q70ClimbingStars.java
@@ -21,13 +21,15 @@ Explanation: There are three ways to climb to the top.
 */
 
 public class Q70ClimbingStars {
-    
+
     public int climbStairs(int n) {
-        if (n == 0 || n == 1) {
+        
+        if (n == 0 || n == 1)
             return 1;
-        }
+        
         int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++) 
+        {
             int temp = curr;
             curr = prev + curr;
             prev = temp;

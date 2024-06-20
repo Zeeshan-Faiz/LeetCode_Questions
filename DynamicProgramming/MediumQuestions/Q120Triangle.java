0edccdeb3b548f029e8e73992e3dd0c15f2aff17
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-20 15:43:18 +0530

    Added Q120Triangle code

--- DynamicProgramming/MediumQuestions/Q120Triangle.java
+++ DynamicProgramming/MediumQuestions/Q120Triangle.java
@@ -35,4 +35,18 @@ public class Q120Triangle {
         }
         return helper(triangle, 0, 0, m, dp);
     }
+
+    public int helper(List<List<Integer>> triangle , int row , int col , int m , int[][] dp){
+        
+        if(row==m)
+            return 0;
+        
+        if(dp[row][col] != -1)
+            return dp[row][col];
+        
+        int  sum1 = triangle.get(row).get(col) + helper(triangle , row+1 , col , m , dp);
+        int  sum2 = triangle.get(row).get(col) + helper(triangle , row+1 , col+1 , m , dp);
+        
+        return dp[row][col] = Math.min(sum1 , sum2);
+    }
 }

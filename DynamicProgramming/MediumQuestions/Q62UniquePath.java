7ca5402ca9221fefba622989cc7ba12379ec2b3f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-20 15:25:59 +0530

    Added Q62UniquePath code

--- DynamicProgramming/MediumQuestions/Q62UniquePath.java
+++ DynamicProgramming/MediumQuestions/Q62UniquePath.java
@@ -1,5 +1,7 @@
 package DynamicProgramming.MediumQuestions;
 
+import java.util.Arrays;
+
 /*
 There is a robot on an m x n grid. The robot is initially located at the top-left corner 
 (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
@@ -23,4 +25,16 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 
 public class Q62UniquePath {
     
+    public int uniquePaths(int m, int n) {
+        int dp [][] = new int [m][n];
+        for (int [] arr : dp) {
+            Arrays.fill(arr,1);
+        }
+        for (int col =1; col <m; col ++) {
+            for (int row =1; row<n; row++) {
+                dp[col][row] = dp[col-1][row] + dp[col][row-1]; 
+            }
+        }
+        return dp[m-1][n-1];
+    }
 }

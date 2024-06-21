3b78c699230e6bdb7cdf66a831cb77ee5f953c99
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-21 09:36:15 +0530

    Added Q931MinFallingPathSum code

--- DynamicProgramming/MediumQuestions/Q931MinFallingPathSum.java
+++ DynamicProgramming/MediumQuestions/Q931MinFallingPathSum.java
@@ -7,7 +7,14 @@ is either directly below or diagonally left/right. Specifically, the next elemen
 (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
 
 Example 1:
+Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+Output: 13
+Explanation: There are two falling paths with a minimum sum as shown.
 
+Example 2:
+Input: matrix = [[-19,57],[-40,-5]]
+Output: -59
+Explanation: The falling path with a minimum sum is shown.
 */
 
 public class Q931MinFallingPathSum {

7a5734fd37632806b647acdf99d3c50290aa98f7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-22 09:47:48 +0530

    Added Q494TargetSum code

--- DynamicProgramming/MediumQuestions/Q494TargetSum.java
+++ DynamicProgramming/MediumQuestions/Q494TargetSum.java
@@ -26,4 +26,21 @@ Output: 1
 
 public class Q494TargetSum {
     
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
+        }
+        if(Math.abs(target)>sum) return 0;
+        if((target+sum)%2==1) return 0;
+        int left=(target+sum)/2;
+        int[] dp=new int[left+1];
+        dp[0]=1;
+        for(int i=0;i<nums.length;i++){
+            for(int j=left;j>=nums[i];j--){
+                dp[j]+=dp[j-nums[i]];
+            }
+        }
+        return dp[left];
+    }
 }

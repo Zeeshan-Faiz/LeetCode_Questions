6be5e61417889e7cb61ca0889e537c5a5f586e4f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-23 14:22:19 +0530

    Added Q1312MinInsertionToMakePalindrome code

--- DynamicProgramming/HardQuestions/Q1312MinInsertionToMakePalindrome.java
+++ DynamicProgramming/HardQuestions/Q1312MinInsertionToMakePalindrome.java
@@ -6,7 +6,19 @@ Return the minimum number of steps to make s palindrome.
 A Palindrome String is one that reads the same backward as well as forward.
 
 Example 1:
+Input: s = "zzazz"
+Output: 0
+Explanation: The string "zzazz" is already palindrome we do not need any insertions.
 
+Example 2:
+Input: s = "mbadm"
+Output: 2
+Explanation: String can be "mbdadbm" or "mdbabdm".
+
+Example 3:
+Input: s = "leetcode"
+Output: 5
+Explanation: Inserting 5 characters the string becomes "leetcodocteel".
 */
 
 public class Q1312MinInsertionToMakePalindrome {

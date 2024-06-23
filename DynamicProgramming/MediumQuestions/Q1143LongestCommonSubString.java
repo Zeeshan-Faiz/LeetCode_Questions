2e33539f7e681f81039e556ef175cd303f8dc6b0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-23 14:00:47 +0530

    Added Q1143LongestCommonSubString code

--- DynamicProgramming/MediumQuestions/Q1143LongestCommonSubString.java
+++ DynamicProgramming/MediumQuestions/Q1143LongestCommonSubString.java
@@ -26,20 +26,24 @@ Explanation: There is no such common subsequence, so the result is 0.
 */
 
 public class Q1143LongestCommonSubString {
-    
+
     public int longestCommonSubsequence(String text1, String text2) {
+        
         char[] s1 = text1.toCharArray();
         char[] s2 = text2.toCharArray();
         return lcsBU(s1, s2);
     }
+
     public static int lcsBU(char[] s1, char[] s2) {
+        
         int[][] dp = new int[s1.length + 1][s2.length + 1];
         for (int i = 1; i <= s1.length; i++) {
             for (int j = 1; j <= s2.length; j++) {
                 int ans = 0;
                 if (s1[i - 1] == s2[j - 1]) {
                     ans = 1 + dp[i - 1][j - 1];
-                } else {
+                } 
+                else {
                     int a = dp[i - 1][j];
                     int b = dp[i][j - 1];
                     ans = Math.max(a, b);

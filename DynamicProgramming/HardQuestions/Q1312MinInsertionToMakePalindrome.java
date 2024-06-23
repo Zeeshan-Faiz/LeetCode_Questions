5104442aa49e6e843f0dcc34f53eb36a9d5c4d2e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-23 14:22:36 +0530

    Added Q1312MinInsertionToMakePalindrome code

--- DynamicProgramming/HardQuestions/Q1312MinInsertionToMakePalindrome.java
+++ DynamicProgramming/HardQuestions/Q1312MinInsertionToMakePalindrome.java
@@ -23,4 +23,23 @@ Explanation: Inserting 5 characters the string becomes "leetcodocteel".
 
 public class Q1312MinInsertionToMakePalindrome {
     
+    public int minInsertions(String s) {
+        char a[] = s.toCharArray();
+        int n = a.length;
+        int dp[] = new int[n];
+        dp[0] = 1;
+        for(int i=1;i<n;i++){
+            dp[i] = 1;
+            int max =0;
+            for(int j=i-1;j>=0;j--){
+                int nxt = dp[j];
+                if(a[i] == a[j]) dp[j] = 2+max;
+                max = Math.max(max,nxt);
+            }
+        }
+        int max =0;
+        for(int i : dp) max = Math.max(i,max);
+        return n - max;
+    }
+}
 }

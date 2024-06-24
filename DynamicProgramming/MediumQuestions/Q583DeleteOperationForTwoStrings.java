c7f40d063fd5a8ffce0964083f8b42300e3f2822
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-24 09:15:01 +0530

    Added Q583DeleteOperationForTwoStrings code

--- DynamicProgramming/MediumQuestions/Q583DeleteOperationForTwoStrings.java
+++ DynamicProgramming/MediumQuestions/Q583DeleteOperationForTwoStrings.java
@@ -1,5 +1,14 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+Given two strings word1 and word2, return the minimum number of steps required to make word1 
+and word2 the same.
+In one step, you can delete exactly one character in either string.
+
+Example 1:
+
+*/
+
 public class Q583DeleteOperationForTwoStrings {
     
 }

a4eeea6b7c669999c9a49c102e787097eda61ca2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-24 09:20:49 +0530

    Added Q1092ShortestCommonSubsequence code

--- DynamicProgramming/HardQuestions/Q1092ShortestCommonSubsequence.java
+++ DynamicProgramming/HardQuestions/Q1092ShortestCommonSubsequence.java
@@ -20,5 +20,53 @@ Output: "aaaaaaaa"
 */
 
 public class Q1092ShortestCommonSubsequence {
-    
+
+    public String shortestCommonSupersequence(String str1, String str2) {
+
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+        int l1 = ch1.length;
+        int l2 = ch2.length;
+        int[][] dp = new int[l1 + 1][l2 + 1];
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if (ch1[i - 1] == ch2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = l1;
+        int j = l2;
+        while (i > 0 && j > 0) {
+            if (ch1[i - 1] == ch2[j - 1]) {
+                sb.append(ch1[i - 1]);
+                i--;
+                j--;
+            } else {
+                if (dp[i][j] == dp[i - 1][j]) {
+                    sb.append(ch1[i - 1]);
+                    i--;
+                } else {
+                    sb.append(ch2[j - 1]);
+                    j--;
+                }
+            }
+        }
+        if (i == 0 && j != 0) {
+            while (j > 0) {
+                sb.append(ch2[j - 1]);
+                j--;
+            }
+        }
+        if (i != 0 && j == 0) {
+            while (i > 0) {
+                sb.append(ch1[i - 1]);
+                i--;
+            }
+        }
+        sb.reverse();
+        return sb.toString();
+    }
 }

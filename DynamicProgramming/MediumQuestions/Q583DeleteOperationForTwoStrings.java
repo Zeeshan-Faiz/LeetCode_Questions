c5f20fc9d1193dff70ab78028738e358ad42c768
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-24 09:15:12 +0530

    Added Q583DeleteOperationForTwoStrings code

--- DynamicProgramming/MediumQuestions/Q583DeleteOperationForTwoStrings.java
+++ DynamicProgramming/MediumQuestions/Q583DeleteOperationForTwoStrings.java
@@ -6,7 +6,13 @@ and word2 the same.
 In one step, you can delete exactly one character in either string.
 
 Example 1:
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
 
+Example 2:
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
 */
 
 public class Q583DeleteOperationForTwoStrings {

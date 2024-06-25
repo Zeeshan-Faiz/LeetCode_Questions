14ae69daa97607e2f295c3fb5a4396c620d8211d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-25 12:49:33 +0530

    Added Q115DistinctSubsequence code

--- DynamicProgramming/HardQuestions/Q115DistinctSubsequence.java
+++ DynamicProgramming/HardQuestions/Q115DistinctSubsequence.java
@@ -1,5 +1,13 @@
 package DynamicProgramming.HardQuestions;
 
+/*
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+The test cases are generated so that the answer fits on a 32-bit signed integer.
+
+Example 1:
+
+*/
+
 public class Q115DistinctSubsequence {
     
 }

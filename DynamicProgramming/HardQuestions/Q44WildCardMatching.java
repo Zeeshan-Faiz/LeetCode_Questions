9e1ea20eb71d5d2010dd91abbab5280fc35fc578
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-25 12:43:25 +0530

    Added Q44WildCardMatching code

--- DynamicProgramming/HardQuestions/Q44WildCardMatching.java
+++ DynamicProgramming/HardQuestions/Q44WildCardMatching.java
@@ -1,5 +1,16 @@
 package DynamicProgramming.HardQuestions;
 
+/*
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for 
+'?' and '*' where:
+    '?' Matches any single character.
+    '*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Example 1:
+
+*/
+
 public class Q44WildCardMatching {
     
 }

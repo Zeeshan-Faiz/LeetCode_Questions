8b22520cd064efb40c2b63db2c80c92ef89f2b43
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-27 12:48:48 +0530

    Added Q188BestTimeToBuyIV code

--- DynamicProgramming/HardQuestions/Q188BestTimeToBuyIV.java
+++ DynamicProgramming/HardQuestions/Q188BestTimeToBuyIV.java
@@ -1,5 +1,17 @@
 package DynamicProgramming.HardQuestions;
 
+/*
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day, 
+and an integer k.
+Find the maximum profit you can achieve. You may complete at most k transactions: i.e. you may buy 
+at most k times and sell at most k times.
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock 
+before you buy again).
+
+Example 1:
+
+*/
+
 public class Q188BestTimeToBuyIV {
     
 }

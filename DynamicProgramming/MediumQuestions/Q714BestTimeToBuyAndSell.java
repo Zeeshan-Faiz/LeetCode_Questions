057fe42e003ef91b290b0266c9f220ee2be24cca
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-28 14:02:21 +0530

    Added Q714BestTimeToBuyAndSell code

--- DynamicProgramming/MediumQuestions/Q714BestTimeToBuyAndSell.java
+++ DynamicProgramming/MediumQuestions/Q714BestTimeToBuyAndSell.java
@@ -25,19 +25,18 @@ Output: 6
 */
 
 public class Q714BestTimeToBuyAndSell {
-    
+
     public int maxProfit(int[] prices, int fee) {
-        int[] buy = new int[prices.length];
-        int[] sell = new int[prices.length];
-        for(int i = 0; i < prices.length; i++){
+        
+        int[] buy = new int[prices.length], sell = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
             buy[i] = Integer.MIN_VALUE;
         }
 
         buy[0] = -prices[0] - fee;
-
-        for(int i = 1; i < prices.length; i++){
-            buy[i] = Math.max(buy[i-1], sell[i-1] - prices[i] - fee);
-            sell[i] = Math.max(sell[i-1], buy[i] + prices[i]);
+        for (int i = 1; i < prices.length; i++) {
+            buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i] - fee);
+            sell[i] = Math.max(sell[i - 1], buy[i] + prices[i]);
         }
 
         return sell[prices.length - 1];

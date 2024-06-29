bf258c53787a34d21fae45cff1cb491f19972596
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-29 15:52:53 +0530

    Added Q368LargestDivisibleSubset code

--- DynamicProgramming/MediumQuestions/Q368LargestDivisibleSubset.java
+++ DynamicProgramming/MediumQuestions/Q368LargestDivisibleSubset.java
@@ -1,5 +1,10 @@
 package DynamicProgramming.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /*
 Given a set of distinct positive integers nums, return the largest subset answer such that every 
 pair (answer[i], answer[j]) of elements in this subset satisfies:
@@ -19,4 +24,30 @@ Output: [1,2,4,8]
 
 public class Q368LargestDivisibleSubset {
     
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int[] parent = new int[nums.length];
+        Arrays.fill(parent, -1);
+        int maxIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    parent[i] = j;
+                    if (dp[i] > dp[maxIndex]) {
+                        maxIndex = i;
+                    }
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        while (maxIndex != -1) {
+            result.add(nums[maxIndex]);
+            maxIndex = parent[maxIndex];
+        }
+        Collections.reverse(result);
+        return result;
+    }
 }

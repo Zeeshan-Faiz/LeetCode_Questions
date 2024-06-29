a959c51703855e2dc6157f7226dfc31fd98deab8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-29 16:02:29 +0530

    Added Q1048LongestStringChain code

--- DynamicProgramming/MediumQuestions/Q1048LongestStringChain.java
+++ DynamicProgramming/MediumQuestions/Q1048LongestStringChain.java
@@ -12,7 +12,20 @@ word chain with k == 1.
 Return the length of the longest possible word chain with words chosen from the given list of words.
 
 Example 1:
+Input: words = ["a","b","ba","bca","bda","bdca"]
+Output: 4
+Explanation: One of the longest word chains is ["a","ba","bda","bdca"].
 
+Example 2:
+Input: words = ["xbc","pcxbcf","xb","cxbc","pcxbc"]
+Output: 5
+Explanation: All the words can be put in a word chain ["xb", "xbc", "cxbc", "pcxbc", "pcxbcf"].
+
+Example 3:
+Input: words = ["abcd","dbqca"]
+Output: 1
+Explanation: The trivial word chain ["abcd"] is one of the longest word chains.
+["abcd","dbqca"] is not a valid word chain because the ordering of the letters is changed.
 */
 
 public class Q1048LongestStringChain {

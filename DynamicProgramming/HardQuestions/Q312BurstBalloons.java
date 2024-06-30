defcd83949208e13c17b25ee47c5707096c52542
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-06-30 19:03:21 +0530

    Added Q312BurstBalloons code

--- DynamicProgramming/HardQuestions/Q312BurstBalloons.java
+++ DynamicProgramming/HardQuestions/Q312BurstBalloons.java
@@ -8,7 +8,15 @@ i + 1 goes out of bounds of the array, then treat it as if there is a balloon wi
 Return the maximum coins you can collect by bursting the balloons wisely.
 
 Example 1:
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
 
+Example 2:
+Input: nums = [1,5]
+Output: 10
 */
 
 public class Q312BurstBalloons {

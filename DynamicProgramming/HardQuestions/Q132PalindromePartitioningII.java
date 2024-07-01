72843ebc90f356533edb7418c04b721d80baa4f2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-01 13:57:51 +0530

    Added Q132PalindromePartitioningII code

--- DynamicProgramming/HardQuestions/Q132PalindromePartitioningII.java
+++ DynamicProgramming/HardQuestions/Q132PalindromePartitioningII.java
@@ -21,16 +21,17 @@ Output: 1
 public class Q132PalindromePartitioningII {
 
     public int minCut(String s) {
+        
         char[] c = s.toCharArray();
         int n = c.length;
         int[] cut = new int[n];
         boolean[][] pal = new boolean[n][n];
-        
-        for(int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++) {
             int min = i;
-            for(int j = 0; j <= i; j++) {
-                if(c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
-                    pal[j][i] = true;  
+            for (int j = 0; j <= i; j++) {
+                if (c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+                    pal[j][i] = true;
                     min = j == 0 ? 0 : Math.min(min, cut[j - 1] + 1);
                 }
             }

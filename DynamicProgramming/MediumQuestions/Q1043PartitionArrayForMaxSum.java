22e61a29b5d0025748d1cd5d601335151bf987d7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-02 15:53:24 +0530

    Added Q1043PartitionArrayForMaxSum code

--- DynamicProgramming/MediumQuestions/Q1043PartitionArrayForMaxSum.java
+++ DynamicProgramming/MediumQuestions/Q1043PartitionArrayForMaxSum.java
@@ -1,5 +1,16 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+Given an integer array arr, partition the array into (contiguous) subarrays of length at most k. 
+After partitioning, each subarray has their values changed to become the maximum value of that 
+subarray.
+Return the largest sum of the given array after partitioning. Test cases are generated so that the 
+answer fits in a 32-bit integer.
+
+Example 1:
+
+*/
+
 public class Q1043PartitionArrayForMaxSum {
     
 }

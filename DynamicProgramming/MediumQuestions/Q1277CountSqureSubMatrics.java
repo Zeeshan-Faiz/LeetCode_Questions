46ba75789cce8bd53ae2cc5bb4febe9b027bfd18
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-02 15:58:09 +0530

    Added Q1277CountSqureSubMatrics code

--- DynamicProgramming/MediumQuestions/Q1277CountSqureSubMatrics.java
+++ DynamicProgramming/MediumQuestions/Q1277CountSqureSubMatrics.java
@@ -1,5 +1,12 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+Example 1:
+
+*/
+
 public class Q1277CountSqureSubMatrics {
     
 }

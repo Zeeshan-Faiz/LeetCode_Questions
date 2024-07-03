a6d920103a51ea6496eb80eceaa745b975e66063
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-03 17:15:22 +0530

    Added Q542Matrix code

--- Graphs/MediumQuestions/Q542Matrix.java
+++ Graphs/MediumQuestions/Q542Matrix.java
@@ -1,5 +1,13 @@
 package Graphs.MediumQuestions;
 
+/*
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+The distance between two adjacent cells is 1.
+
+Example 1:
+
+*/
+
 public class Q542Matrix {
     
 }

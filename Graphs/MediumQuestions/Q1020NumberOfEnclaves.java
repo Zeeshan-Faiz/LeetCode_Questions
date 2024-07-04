5f94ce327f015cdd229fe68ee99456e308f92ed6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-04 14:31:03 +0530

    Added Q1020NumberOfEnclaves code

--- Graphs/MediumQuestions/Q1020NumberOfEnclaves.java
+++ Graphs/MediumQuestions/Q1020NumberOfEnclaves.java
@@ -19,33 +19,47 @@ Explanation: All 1s are either on the boundary or can reach the boundary.
 */
 
 public class Q1020NumberOfEnclaves {
-    
+
     public int numEnclaves(int[][] grid) {
+        
         int m = grid.length;
         int n = grid[0].length;
-        if(m<=2 || n<=2) return 0;
-        for(int i=0; i<m; i++){
-            if(grid[i][0]==1){
+        if (m <= 2 || n <= 2)
+            return 0;
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1) {
                 dfs(grid, i, 0);
             }
-            if(grid[i][n-1]==1){
-                dfs(grid, i, n-1);
+            if (grid[i][n - 1] == 1) {
+                dfs(grid, i, n - 1);
             }
         }
-        for(int j=0; j<n; j++){
-            if(grid[0][j]==1){
+        for (int j = 0; j < n; j++) {
+            if (grid[0][j] == 1) {
                 dfs(grid, 0, j);
             }
-            if(grid[m-1][j]==1){
-                dfs(grid, m-1, j);
+            if (grid[m - 1][j] == 1) {
+                dfs(grid, m - 1, j);
             }
         }
-        int ans=0;
-        for(int i=1; i<m-1; i++){
-            for(int j=1; j<n-1; j++){
-                ans+=grid[i][j];
+        int ans = 0;
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                ans += grid[i][j];
             }
         }
         return ans;
+    }
+
+    public void dfs(int[][] grid, int i, int j) {
+        
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != 1)
+            return;
+
+        grid[i][j] = 0;
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+        dfs(grid, i + 1, j);
     }
 }

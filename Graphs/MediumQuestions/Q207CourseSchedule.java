becdd20a13334d28b9c66f6e8a1600130b617fa9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-06 17:11:45 +0530

    Added Q207CourseSchedule code

--- Graphs/MediumQuestions/Q207CourseSchedule.java
+++ Graphs/MediumQuestions/Q207CourseSchedule.java
@@ -23,4 +23,33 @@ To take course 1 you should have finished course 0, and to take course 0 you sho
 
 public class Q207CourseSchedule {
     
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(numCourses == 0){
+            return true;
+        }
+         int[] course = new int[numCourses];
+         for (int i = 0; i < prerequisites.length; i++){
+            course[prerequisites[i][1]]++;
+         } 
+         boolean visited[] = new boolean[prerequisites.length];
+         boolean flag = true;
+         while (flag) {
+             flag = false;
+             for (int i = 0; i < prerequisites.length; i++) {
+                 if (!visited[i]) {
+                     if (course[prerequisites[i][0]] == 0) {
+                         visited[i] = true;
+                         course[prerequisites[i][1]]--;
+                         flag = true;
+                     }
+                 }
+             }
+         }
+         for(int i = 0; i < numCourses; i++){
+            if(course[i] != 0) {
+                return false;
+            }
+         } 
+         return true;
+     }
 }

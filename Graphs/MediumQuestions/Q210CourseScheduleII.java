205f9e6193b1c8109eca6703e58348f58476d441
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-06 17:06:32 +0530

    Added Q210CourseScheduleII code

--- Graphs/MediumQuestions/Q210CourseScheduleII.java
+++ Graphs/MediumQuestions/Q210CourseScheduleII.java
@@ -10,7 +10,18 @@ Return the ordering of courses you should take to finish all courses. If there a
 answers, return any of them. If it is impossible to finish all courses, return an empty array.
 
 Example 1:
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
 
+Example 2:
+Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
+So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+
+Example 3:
+Input: numCourses = 1, prerequisites = []
+Output: [0]
 */
 
 public class Q210CourseScheduleII {

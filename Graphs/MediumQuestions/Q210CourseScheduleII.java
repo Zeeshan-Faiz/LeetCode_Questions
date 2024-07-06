74336db13eb529eb8090e0d43bf49a84438aa489
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-06 17:06:47 +0530

    Added Q210CourseScheduleII code

--- Graphs/MediumQuestions/Q210CourseScheduleII.java
+++ Graphs/MediumQuestions/Q210CourseScheduleII.java
@@ -1,5 +1,7 @@
 package Graphs.MediumQuestions;
 
+import java.util.ArrayList;
+
 /*
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You 
 are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take 
@@ -26,4 +28,49 @@ Output: [0]
 
 public class Q210CourseScheduleII {
     
+    int[] ans;
+    boolean[] taken;
+    ArrayList<ArrayList<Integer>> graph;
+    int ansIdx;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> prereqs = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            prereqs.add(new ArrayList<Integer>());
+        }
+        for (int[] p : prerequisites) {
+            prereqs.get(p[0]).add(p[1]);
+        }
+        this.taken = new boolean[numCourses];
+        this.ans = new int[numCourses];
+        this.graph = prereqs;
+        this.ansIdx = -1;
+        // System.out.println(graph.size());
+        for (int i = 0; i < numCourses; i++) {
+            boolean search = dfs(i,0);
+            // System.out.println(search);
+            if (!search) return new int[0];
+        }
+        if (ansIdx > numCourses) return new int[0];
+        return ans;
+    }
+
+    public boolean dfs(int course, int depth) {
+        // System.out.println(depth);
+        if (depth >= graph.size()) {
+            // System.out.println("returning false");
+            return false;
+        } 
+        if (taken[course]) return true;
+        ArrayList<Integer> prereqs = graph.get(course);
+        for (int i = 0; i < prereqs.size(); i++) {
+            if (!taken[prereqs.get(i)]) {
+                if (!dfs(prereqs.get(i), depth + 1)) return false;
+            }
+        }
+        taken[course] = true;
+        ansIdx++;
+        if (ansIdx >= graph.size()) return false;
+        ans[ansIdx] = course;
+        return true;
+    }
 }

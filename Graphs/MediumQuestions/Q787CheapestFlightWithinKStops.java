e3b458e77cf14ec337df1aacb0509f53f63dd25f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-08 16:56:36 +0530

    Added Q787CheapestFlightWithinKStops code

--- Graphs/MediumQuestions/Q787CheapestFlightWithinKStops.java
+++ Graphs/MediumQuestions/Q787CheapestFlightWithinKStops.java
@@ -1,5 +1,16 @@
 package Graphs.MediumQuestions;
 
+/*
+There are n cities connected by some number of flights. You are given an array flights where 
+flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi 
+with cost price.
+You are also given three integers src, dst, and k, return the cheapest price from src to dst 
+with at most k stops. If there is no such route, return -1.
+
+Example 1:
+
+*/
+
 public class Q787CheapestFlightWithinKStops {
     
 }

b3c23ff854fde3b524fae393c7c313dad47d3e07
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-08 16:57:13 +0530

    Added Q787CheapestFlightWithinKStops code

--- Graphs/MediumQuestions/Q787CheapestFlightWithinKStops.java
+++ Graphs/MediumQuestions/Q787CheapestFlightWithinKStops.java
@@ -1,5 +1,7 @@
 package Graphs.MediumQuestions;
 
+import java.util.Arrays;
+
 /*
 There are n cities connected by some number of flights. You are given an array flights where 
 flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi 
@@ -32,4 +34,34 @@ The optimal path with no stops from city 0 to 2 is marked in red and has cost 50
 
 public class Q787CheapestFlightWithinKStops {
     
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] distance = new int[n];
+    Arrays.fill(distance, Integer.MAX_VALUE);
+    distance[src] = 0;
+    
+    for (int i = 0; i <= k; i++) {
+        if (isRoutePossible(distance, flights)) {
+            break; 
+                    }
+    }
+    
+    return distance[dst] == Integer.MAX_VALUE ? -1 : distance[dst];
+}
+
+    private boolean isRoutePossible(int[] dist, int[][] flights) {
+        int[] copy = Arrays.copyOf(dist, dist.length);
+        boolean result = true;
+
+        for (int[] flight : flights) {
+            int src = flight[0];
+            int dst = flight[1];
+            int cost = flight[2];
+
+            if (copy[src] != Integer.MAX_VALUE && dist[dst] > copy[src] + cost) {
+                dist[dst] = cost + copy[src];
+                result = false;
+            }
+        }
+        return result;
+    }
 }

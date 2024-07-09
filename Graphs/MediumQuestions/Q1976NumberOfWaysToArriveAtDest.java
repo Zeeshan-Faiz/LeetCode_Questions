7bc10f297f8d898b87fd586620e7067701e811fe
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-09 09:54:56 +0530

    Added Q1976NumberOfWaysToArriveAtDest code

--- Graphs/MediumQuestions/Q1976NumberOfWaysToArriveAtDest.java
+++ Graphs/MediumQuestions/Q1976NumberOfWaysToArriveAtDest.java
@@ -13,7 +13,19 @@ Return the number of ways you can arrive at your destination in the shortest amo
 Since the answer may be large, return it modulo 109 + 7.
 
 Example 1:
+Input: n = 7, roads = [[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]]
+Output: 4
+Explanation: The shortest amount of time it takes to go from intersection 0 to intersection 6 is 7 minutes.
+The four ways to get there in 7 minutes are:
+- 0 ➝ 6
+- 0 ➝ 4 ➝ 6
+- 0 ➝ 1 ➝ 2 ➝ 5 ➝ 6
+- 0 ➝ 1 ➝ 3 ➝ 5 ➝ 6
 
+Example 2:
+Input: n = 2, roads = [[1,0,10]]
+Output: 1
+Explanation: There is only one way to go from intersection 0 to intersection 1, and it takes 10 minutes.
 */
 
 public class Q1976NumberOfWaysToArriveAtDest {

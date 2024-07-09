5f3b4d1a33004e705594f20c3b4672327baa7a31
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-09 09:40:21 +0530

    Added Q743NetworkDelayTime code

--- Graphs/MediumQuestions/Q743NetworkDelayTime.java
+++ Graphs/MediumQuestions/Q743NetworkDelayTime.java
@@ -8,7 +8,16 @@ We will send a signal from a given node k. Return the minimum time it takes for 
 receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
 
 Example 1:
+Input: times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+Output: 2
 
+Example 2:
+Input: times = [[1,2,1]], n = 2, k = 1
+Output: 1
+
+Example 3:
+Input: times = [[1,2,1]], n = 2, k = 2
+Output: -1
 */
 
 public class Q743NetworkDelayTime {

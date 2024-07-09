3c532efc3d08b2e7fee692791234358ccda32e0f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-09 09:55:12 +0530

    Added Q1976NumberOfWaysToArriveAtDest code

--- Graphs/MediumQuestions/Q1976NumberOfWaysToArriveAtDest.java
+++ Graphs/MediumQuestions/Q1976NumberOfWaysToArriveAtDest.java
@@ -1,5 +1,7 @@
 package Graphs.MediumQuestions;
 
+import java.util.Arrays;
+
 /*
 You are in a city that consists of n intersections numbered from 0 to n - 1 with bi-directional 
 roads between some intersections. The inputs are generated such that you can reach any 
@@ -30,4 +32,46 @@ Explanation: There is only one way to go from intersection 0 to intersection 1, 
 
 public class Q1976NumberOfWaysToArriveAtDest {
     
+    public int countPaths(int n, int[][] roads) {
+        final long inf = Long.MAX_VALUE / 2;
+        final int mod = (int) 1e9 + 7;
+        long[][] g = new long[n][n];
+        for (var e : g) {
+            Arrays.fill(e, inf);
+        }
+        for (var r : roads) {
+            int u = r[0], v = r[1], t = r[2];
+            g[u][v] = t;
+            g[v][u] = t;
+        }
+        g[0][0] = 0;
+        long[] dist = new long[n];
+        Arrays.fill(dist, inf);
+        dist[0] = 0;
+        long[] f = new long[n];
+        f[0] = 1;
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; ++i) {
+            int t = -1;
+            for (int j = 0; j < n; ++j) {
+                if (!vis[j] && (t == -1 || dist[j] < dist[t])) {
+                    t = j;
+                }
+            }
+            vis[t] = true;
+            for (int j = 0; j < n; ++j) {
+                if (j == t) {
+                    continue;
+                }
+                long ne = dist[t] + g[t][j];
+                if (dist[j] > ne) {
+                    dist[j] = ne;
+                    f[j] = f[t];
+                } else if (dist[j] == ne) {
+                    f[j] = (f[j] + f[t]) % mod;
+                }
+            }
+        }
+        return (int) f[n - 1];
+    }
 }
